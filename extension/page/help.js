b49b4dad79afa6d96f4786656b0c14b5feb279a3
Jan Malakhovski <oxij@oxij.org>
2023-08-23 13:12:00 +0000

    extension/page/help: make highlight sticky on click

--- extension/page/help.js
+++ extension/page/help.js
@@ -14,16 +14,23 @@ document.addEventListener("DOMContentLoaded", () => {
     iframe.src = popupURL + "#all";
     document.body.appendChild(iframe);
 
+    // a flag making the highlight stick around
+    let sticky = false;
+
     // broadcast highlight messages on mouseovers over links to popup.html
     for (let el of document.getElementsByTagName("a")) {
         if (!el.href.startsWith(popupURL + "#")) continue;
         let target = el.href.substr(popupURL.length + 1);
         el.href = "#";
+        el.onclick = (event) => {
+            sticky = !sticky;
+        };
         el.onmouseover = (event) => {
             browser.runtime.sendMessage(["broadcast", ["highlight", target]]);
         };
         el.onmouseleave = (event) => {
-            browser.runtime.sendMessage(["broadcast", ["highlight", "all"]]);
+            if (!sticky)
+                browser.runtime.sendMessage(["broadcast", ["highlight", "all"]]);
         };
     }
 
