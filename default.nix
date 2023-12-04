98179a440fa9adc024a1b9fcdb3910680a53262b
Jan Malakhovski <oxij@oxij.org>
2023-12-04 20:00:00 +0000

    default.nix: bump version

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 let packages = import ./packages.nix { inherit pkgs; }; in
 
 pkgs.buildEnv {
-  name = "pwebarc-20231122";
+  name = "pwebarc-20231204";
   paths = with packages; [
     dumb_server
     extension
