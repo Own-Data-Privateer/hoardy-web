Own-Data-Privateer/hoardy-web

commit 58db8607b021d5b560762b56feb804c6192be372

tool: refactor, make all `organize` operations atomic
- split deferred disk IO away from file handling logic, mostly;
- add `--cache-number` option;
- rename and reimplement `organize --action`s:
  - `--action rename` -> `--move`, making it more robust (it now does hard`link`
    or `copy` followed by an `unlink` deferred until after all `fsync`s);
  - new action: `--copy`;
  - `--action hardlink` -> `--hardlink`;
  - `--action symlink` -> `--symlink`;
  - `--action symlink-update` -> `--symlink --latest`;
  - make all of them atomic w.r.t. disk IO and stdout (`--*-terminated`) logs;
  - make all of them de-duplicating and non-duplicating when possible.

tool/wrrarms/io.py
@@ -1,0 +1,329 @@
+# Copyright (c) 2024 Jan Malakhovski <oxij@oxij.org>
+#
+# This file is a part of pwebarc project.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import dataclasses as _dc
+import errno as _errno
+import fcntl as _fcntl
+import io as _io
+import os as _os
+import shutil as _shutil
+import sys as _sys
+import typing as _t
+
+from gettext import gettext, ngettext
+
+from .wrr import *
+
+def fsync_maybe(fd : int) -> None:
+    try:
+        _os.fsync(fd)
+    except OSError as exc:
+        if exc.errno != _errno.EINVAL:
+            raise exc
+        # EINVAL means fd is not attached to a file, so we
+        # ignore this error
+
+def fsync_fpath(fpath : _t.AnyStr, flags : int = 0) -> None:
+    fd = _os.open(fpath, _os.O_RDONLY | flags)
+    try:
+        _os.fsync(fd)
+    finally:
+        _os.close(fd)
+
+def handle_ENAMETOOLONG(exc : OSError, name : _t.AnyStr) -> None:
+    if exc.errno == _errno.ENAMETOOLONG:
+        raise Failure(gettext(f"target file system rejects `%s` as too long: either one of the path components is longer than the maximum allowed file name on the target file system or the whole thing is longer than kernel MAX_PATH"), name)
+
+def fileobj_content_equals(f : _io.BufferedReader, data : bytes) -> bool:
+    # TODO more efficiently
+    fdata = f.read()
+    return fdata == data
+
+def file_content_equals(path : _t.AnyStr, data : bytes) -> bool:
+    try:
+        with open(path, "rb") as f:
+            return fileobj_content_equals(f, data)
+    except FileNotFoundError:
+        return False
+
+@_dc.dataclass
+class DeferredSync(_t.Generic[_t.AnyStr]):
+    """Deferred file system syncs and unlinks."""
+
+    replaces : list[tuple[_t.AnyStr, _t.AnyStr]] = _dc.field(default_factory=list)
+    files : set[_t.AnyStr] = _dc.field(default_factory=set)
+    dirs : set[_t.AnyStr] = _dc.field(default_factory=set)
+    unlinks : set[_t.AnyStr] = _dc.field(default_factory=set)
+
+    def sync(self) -> None:
+        if len(self.replaces) > 0:
+            for fpath, _ in self.replaces:
+                fsync_fpath(fpath)
+
+            for ffrom, fto in self.replaces:
+                _os.replace(ffrom, fto)
+            self.replaces = []
+
+        if len(self.files) > 0:
+            for fpath in self.files:
+                fsync_fpath(fpath)
+            self.files = set()
+
+        if len(self.dirs) > 0:
+            for fpath in self.dirs:
+                fsync_fpath(fpath, _os.O_DIRECTORY)
+            self.dirs = set()
+
+    def finish(self) -> None:
+        self.sync()
+
+        if len(self.unlinks) > 0:
+            for fpath in self.unlinks:
+                try:
+                    _os.unlink(fpath)
+                except Exception:
+                    pass
+            self.unlinks = set()
+
+def make_file(make_dst : _t.Callable[[_t.AnyStr], None], dst : _t.AnyStr,
+              dsync : DeferredSync[_t.AnyStr] | None = None,
+              do_replace : bool = False) -> None:
+    if not do_replace and _os.path.lexists(dst):
+        # fail early
+        raise FileExistsError(_errno.EEXIST, _os.strerror(_errno.EEXIST), dst)
+
+    dirname = _os.path.dirname(dst)
+
+    make_dst(dst)
+
+    if dsync is None:
+        fsync_fpath(dst)
+        fsync_fpath(dirname, _os.O_DIRECTORY)
+    else:
+        dsync.files.add(dst)
+        dsync.dirs.add(dirname)
+
+def atomic_make_file(make_dst : _t.Callable[[_t.AnyStr], None], dst : _t.AnyStr,
+                     dsync : DeferredSync[_t.AnyStr] | None = None,
+                     do_replace : bool = False) -> None:
+    if not do_replace and _os.path.lexists(dst):
+        # fail early
+        raise FileExistsError(_errno.EEXIST, _os.strerror(_errno.EEXIST), dst)
+
+    dirname = _os.path.dirname(dst)
+    if isinstance(dst, str):
+        dst_part = dst + ".part"
+    else:
+        dst_part = dst + b".part"
+
+    make_dst(dst_part)
+
+    if dsync is None:
+        fsync_fpath(dst_part)
+
+    dirfd = _os.open(dirname, _os.O_RDONLY | _os.O_DIRECTORY)
+    if _sys.platform != "win32":
+        _fcntl.flock(dirfd, _fcntl.LOCK_EX)
+
+    try:
+        # this is now atomic on POSIX
+        if not do_replace and _os.path.lexists(dst):
+            raise FileExistsError(_errno.EEXIST, _os.strerror(_errno.EEXIST), dst)
+
+        if dsync is None:
+            _os.replace(dst_part, dst)
+            _os.fsync(dirfd)
+        else:
+            dsync.replaces.append((dst_part, dst))
+            dsync.dirs.add(dirname)
+    finally:
+        if _sys.platform != "win32":
+            _fcntl.flock(dirfd, _fcntl.LOCK_UN)
+        _os.close(dirfd)
+
+def atomic_copy2(src : _t.AnyStr, dst : _t.AnyStr,
+                 dsync : DeferredSync[_t.AnyStr] | None = None,
+                 do_replace : bool = False,
+                 follow_symlinks : bool = True) -> None:
+    def make_dst(dst_part : _t.AnyStr) -> None:
+        if not follow_symlinks and _os.path.islink(src):
+            _os.symlink(_os.readlink(src), dst_part)
+        else:
+            with open(src, "rb") as fsrc:
+                try:
+                    with open(dst_part, "xb") as fdst:
+                        _shutil.copyfileobj(fsrc, fdst)
+                except Exception as exc:
+                    try: _os.unlink(dst_part)
+                    except Exception: pass
+                    raise exc
+        _shutil.copystat(src, dst_part, follow_symlinks = follow_symlinks)
+
+    # always use the atomic version here, like rsync does,
+    # since copying can be interrupted in the middle
+    atomic_make_file(make_dst, dst, dsync, do_replace)
+
+def atomic_link(src : _t.AnyStr, dst : _t.AnyStr,
+                dsync : DeferredSync[_t.AnyStr] | None = None,
+                do_replace : bool = False,
+                follow_symlinks : bool = True) -> None:
+    if follow_symlinks and _os.path.islink(src):
+        src = _os.path.realpath(src)
+
+    def make_dst(dst_part : _t.AnyStr) -> None:
+        _os.link(src, dst_part, follow_symlinks = follow_symlinks)
+
+    # _os.link is atomic, so non-atomic make_file is ok
+    if do_replace:
+        atomic_make_file(make_dst, dst, dsync, do_replace)
+    else:
+        make_file(make_dst, dst, dsync, do_replace)
+
+def atomic_symlink(src : _t.AnyStr, dst : _t.AnyStr,
+                   dsync : DeferredSync[_t.AnyStr] | None = None,
+                   do_replace : bool = False,
+                   follow_symlinks : bool = True) -> None:
+    if follow_symlinks and _os.path.islink(src):
+        src = _os.path.realpath(src)
+
+    def make_dst(dst_part : _t.AnyStr) -> None:
+        _os.symlink(src, dst_part)
+
+    # _os.symlink is atomic, so non-atomic make_file is ok
+    if do_replace:
+        atomic_make_file(make_dst, dst, dsync, do_replace)
+    else:
+        make_file(make_dst, dst, dsync, do_replace)
+
+def atomic_link_or_copy2(src : _t.AnyStr, dst : _t.AnyStr,
+                         dsync : DeferredSync[_t.AnyStr] | None = None,
+                         do_replace : bool = False,
+                         follow_symlinks : bool = True) -> None:
+    try:
+        atomic_link(src, dst, dsync, do_replace, follow_symlinks)
+    except OSError as exc:
+        if exc.errno != _errno.EXDEV:
+            raise exc
+        atomic_copy2(src, dst, dsync, do_replace, follow_symlinks)
+
+def atomic_move(src : _t.AnyStr, dst : _t.AnyStr,
+                dsync : DeferredSync[_t.AnyStr] | None = None,
+                do_replace : bool = False,
+                follow_symlinks : bool = True) -> None:
+    atomic_link_or_copy2(src, dst, dsync, do_replace, follow_symlinks)
+    if dsync is None:
+        _os.unlink(src)
+    else:
+        dsync.unlinks.add(src)
+
+DataSource = _t.TypeVar("DataSource")
+class DeferredIO(_t.Generic[DataSource, _t.AnyStr]):
+    """A deferred IO operation over abs_out_path : _.AnyStr, which uses a DataSource
+       that has ReqresExpr inside as the source of data to be IO'd.
+
+       This exists for efficiently reasons: to eliminate away consequent
+       `os.rename` and `os.symlink` calls to the same target when updating FS
+       paths, and also so that disk writes could be batched.
+    """
+
+    def format_source(self) -> str | bytes:
+        raise NotImplementedError()
+
+    @staticmethod
+    def defer(abs_out_path : _t.AnyStr, out_source : DataSource | None,
+              in_source: DataSource, rrexpr : ReqresExpr) \
+            -> tuple[_t.Any | None, DataSource | None, bool]:
+            #     ^ this is _t.Self
+        raise NotImplementedError()
+
+    def update_from(self, in_source : DataSource, rrexpr : ReqresExpr) \
+            -> tuple[DataSource | None, bool]:
+        raise NotImplementedError()
+
+    def run(self, abs_out_path : _t.AnyStr,
+            dsync : DeferredSync[_t.AnyStr] | None = None,
+            dry_run : bool = False) \
+            -> DataSource | None:
+        raise NotImplementedError()
+
+@_dc.dataclass
+class SourcedData(_t.Generic[_t.AnyStr]):
+    source : _t.AnyStr
+    data : bytes
+
+    def anystr(self) -> _t.AnyStr:
+        return self.source
+
+@_dc.dataclass
+class DeferredFileWrite(DeferredIO[SourcedData[_t.AnyStr], _t.AnyStr], _t.Generic[_t.AnyStr]):
+    source : _t.AnyStr
+    data : bytes                                # file contents
+    changed : bool = _dc.field(default = False) # was it updated?
+
+    def format_source(self) -> str | bytes:
+        return self.source
+
+    @staticmethod
+    def defer(abs_out_path : _t.AnyStr, out_source : SourcedData[_t.AnyStr] | None,
+              in_source : SourcedData[_t.AnyStr], rrexpr : ReqresExpr) \
+            -> tuple[_t.Any | None, None, bool]:
+        assert out_source is None
+
+        if file_content_equals(abs_out_path, in_source.data):
+            # same data on disk, don't even generate an intent
+            return None, None, True
+
+        return DeferredFileWrite(in_source.source, in_source.data), None, True
+
+    def update_from(self, in_source : SourcedData[_t.AnyStr], rrexpr : ReqresExpr) -> tuple[None, bool]:
+        # yes, just replace it and don't cache anything
+        self.source = in_source.source
+        self.data = in_source.data
+        self.changed = True
+        return None, True
+
+    def run(self, abs_out_path : _t.AnyStr, dsync : DeferredSync[_t.AnyStr] | None = None, dry_run : bool = False) -> None:
+        if dry_run:
+            return None
+
+        if self.changed and file_content_equals(abs_out_path, self.data):
+            # same data on disk
+            return None
+
+        dirname = _os.path.dirname(abs_out_path)
+        try:
+            _os.makedirs(dirname, exist_ok = True)
+        except OSError as exc:
+            handle_ENAMETOOLONG(exc, dirname)
+            raise exc
+
+        def make_dst(dst_part : _t.AnyStr) -> None:
+            try:
+                with open(dst_part, "xb") as f:
+                    f.write(self.data)
+            except FileExistsError:
+                raise Failure(gettext(f"trying to overwrite `%s` which already exists"), dst_part)
+
+        try:
+            atomic_make_file(make_dst, abs_out_path, dsync, True)
+        except FileExistsError:
+            raise Failure(gettext(f"trying to overwrite `%s` which already exists"), abs_out_path)
+        except OSError as exc:
+            handle_ENAMETOOLONG(exc, dirname)
+            raise exc
+
+        return None

tool/wrrarms/mitmproxy.py
@@ -31,7 +31,7 @@ def _hd(x : _t.Any) -> Headers:
         res.append((k.decode("ascii"), v))
     return Headers(res)
 
-def load_as_wrrs(fobj : _io.BufferedReader) -> _t.Iterator[Reqres]:
+def load_as_wrrs(fobj : _io.BufferedReader, abs_path : _t.AnyStr) -> _t.Iterator[Reqres]:
     stream = mitmproxy.io.FlowReader(fobj).stream()
     for flow in stream:
         if not isinstance(flow, mitmproxy.http.HTTPFlow):
@@ -98,7 +98,7 @@ def load_as_wrrs(fobj : _io.BufferedReader) -> _t.Iterator[Reqres]:
             for msg in ws.messages:
                 if type(msg.content) is bytes:
                     content = msg.content
-                elif type(msg.content) is str: # type: ignore
+                elif type(msg.content) is str:
                     # even though mitmproxy declares content to be `bytes`, reading
                     # dump files produced by old mitmproxy can produce `str`
                     content = msg.content.encode("utf-8") # type: ignore

tool/wrrarms/wrr.py
@@ -94,7 +94,7 @@ Reqres_fields = {
 }
 
 Reqres_derived_attrs = {
-    "fs_path": "file system path for the WRR file containing this reqres; str",
+    "fs_path": "file system path for the WRR file containing this reqres; str or None",
 
     "qtime": 'aliast for `request.started_at`; mnemonic: "reQuest TIME"; seconds since UNIX epoch; decimal float',
     "qtime_ms": "`qtime` in milliseconds rounded down to nearest integer; milliseconds since UNIX epoch; int",
@@ -201,11 +201,11 @@ class ReqresExpr:
     reqres : Reqres
     items : dict[str, _t.Any]
 
-    def __init__(self, reqres : Reqres, path : str) -> None:
+    def __init__(self, reqres : Reqres, path : str | bytes | None = None) -> None:
         self.reqres = reqres
-        self.items = {
-            "fs_path": path
-        }
+        self.items = {}
+        if path is not None:
+            self.items["fs_path"] = path
 
     def _fill_time(self, prefix : str, ts : Epoch) -> None:
         dt = _time.gmtime(int(ts))
@@ -388,7 +388,7 @@ def trivial_Reqres(url : str) -> Reqres:
 
 def test_ReqresExpr() -> None:
     def mk(url : str) -> ReqresExpr:
-        return ReqresExpr(trivial_Reqres(url), "/homeless-shelter/trivial")
+        return ReqresExpr(trivial_Reqres(url), None)
 
     def check(x : ReqresExpr, name : str, value : _t.Any) -> None:
         if x[name] != value:
@@ -504,9 +504,17 @@ def wrr_load(fobj : _io.BufferedReader) -> Reqres:
     else:
         raise ParsingError("can't parse CBOR data: unknown format %s", data[0])
 
+def wrr_load_expr(fobj : _io.BufferedReader, path : str | bytes) -> ReqresExpr:
+    reqres = wrr_load(fobj)
+    return ReqresExpr(reqres, path)
+
 def wrr_loadf(path : str | bytes) -> Reqres:
     with open(path, "rb") as f:
         return wrr_load(f)
+
+def wrr_loadf_expr(path : str | bytes) -> ReqresExpr:
+    reqres = wrr_loadf(path)
+    return ReqresExpr(reqres, path)
 
 def wrr_dumps(reqres : Reqres, compress : bool = True) -> bytes:
     req = reqres.request

commit 93d4d7fee06f7441bf17292e40dddb69d08eeb3f

tool: import mitmproxy: forbid file overwrites
Fixes an issue introduced in 58db8607b021d5b560762b56feb804c6192be372.

tool/wrrarms/__main__.py
@@ -613,6 +613,7 @@ def make_organize_emit(cargs : _t.Any, destination : str, allow_updates : bool) 
                     return self.source, True
 
                 # check if overwriting it would be a noop
+                # TODO more efficiently
                 with open(self.source.abs_path, "rb") as f:
                     disk_data = f.read()
 
@@ -722,7 +723,7 @@ def cmd_import_mitmproxy(cargs : _t.Any) -> None:
     slurp_stdin0(cargs)
     handle_sorting(cargs)
 
-    emit_one, finish = make_deferred_emit(cargs, cargs.destination, "import", "importing", DeferredFileWrite)
+    emit_one, finish = make_deferred_emit(cargs, cargs.destination, "import", "importing", DeferredFileNoOverwrite)
 
     def emit(abs_in_path : str, rel_in_path : str, in_stat : _os.stat_result, rr : _t.Iterator[Reqres]) -> None:
         dev, ino = in_stat.st_dev, in_stat.st_ino

tool/wrrarms/io.py
@@ -260,6 +260,28 @@ class DeferredIO(_t.Generic[DataSource, _t.AnyStr]):
             -> DataSource | None:
         raise NotImplementedError()
 
+def undeferred_write(data : bytes, dst : _t.AnyStr,
+                     dsync : DeferredSync[_t.AnyStr] | None = None,
+                     do_replace : bool = False) -> None:
+    dirname = _os.path.dirname(dst)
+    try:
+        _os.makedirs(dirname, exist_ok = True)
+    except OSError as exc:
+        handle_ENAMETOOLONG(exc, dirname)
+        raise exc
+
+    def make_dst(dst_part : _t.AnyStr) -> None:
+        with open(dst_part, "xb") as f:
+            f.write(data)
+
+    try:
+        atomic_make_file(make_dst, dst, dsync, do_replace)
+    except FileExistsError as exc:
+        raise Failure(gettext(f"trying to overwrite `%s` which already exists"), exc.filename)
+    except OSError as exc:
+        handle_ENAMETOOLONG(exc, dst)
+        raise exc
+
 @_dc.dataclass
 class SourcedData(_t.Generic[_t.AnyStr]):
     source : _t.AnyStr
@@ -267,6 +289,37 @@ class SourcedData(_t.Generic[_t.AnyStr]):
 
     def anystr(self) -> _t.AnyStr:
         return self.source
+
+@_dc.dataclass
+class DeferredFileNoOverwrite(DeferredIO[SourcedData[_t.AnyStr], _t.AnyStr], _t.Generic[_t.AnyStr]):
+    source : _t.AnyStr
+    data : bytes # file contents
+
+    def format_source(self) -> str | bytes:
+        return self.source
+
+    @staticmethod
+    def defer(abs_out_path : _t.AnyStr, out_source : SourcedData[_t.AnyStr] | None,
+              in_source : SourcedData[_t.AnyStr], rrexpr : ReqresExpr) \
+            -> tuple[_t.Any | None, None, bool]:
+        assert out_source is None
+
+        try:
+            with open(abs_out_path, "rb") as f:
+                eq = fileobj_content_equals(f, in_source.data)
+            return None, None, eq
+        except FileNotFoundError:
+            return DeferredFileNoOverwrite(in_source.source, in_source.data), None, True
+        except OSError as exc:
+            handle_ENAMETOOLONG(exc, abs_out_path)
+            raise exc
+
+    def update_from(self, in_source : SourcedData[_t.AnyStr], rrexpr : ReqresExpr) -> tuple[None, bool]:
+        return None, self.source == in_source.source
+
+    def run(self, abs_out_path : _t.AnyStr, dsync : DeferredSync[_t.AnyStr] | None = None, dry_run : bool = False) -> None:
+        if dry_run: return
+        undeferred_write(self.data, abs_out_path, dsync, False)
 
 @_dc.dataclass
 class DeferredFileWrite(DeferredIO[SourcedData[_t.AnyStr], _t.AnyStr], _t.Generic[_t.AnyStr]):
@@ -283,14 +336,22 @@ class DeferredFileWrite(DeferredIO[SourcedData[_t.AnyStr], _t.AnyStr], _t.Generi
             -> tuple[_t.Any | None, None, bool]:
         assert out_source is None
 
-        if file_content_equals(abs_out_path, in_source.data):
-            # same data on disk, don't even generate an intent
-            return None, None, True
+        try:
+            if file_content_equals(abs_out_path, in_source.data):
+                # same data on disk, don't even generate an intent
+                return None, None, True
+        except OSError as exc:
+            handle_ENAMETOOLONG(exc, abs_out_path)
+            raise exc
 
         return DeferredFileWrite(in_source.source, in_source.data), None, True
 
     def update_from(self, in_source : SourcedData[_t.AnyStr], rrexpr : ReqresExpr) -> tuple[None, bool]:
-        # yes, just replace it and don't cache anything
+        if self.data == in_source.data:
+            # same data
+            return None, True
+
+        # update
         self.source = in_source.source
         self.data = in_source.data
         self.changed = True
@@ -298,32 +359,10 @@ class DeferredFileWrite(DeferredIO[SourcedData[_t.AnyStr], _t.AnyStr], _t.Generi
 
     def run(self, abs_out_path : _t.AnyStr, dsync : DeferredSync[_t.AnyStr] | None = None, dry_run : bool = False) -> None:
         if dry_run:
-            return None
+            return
 
         if self.changed and file_content_equals(abs_out_path, self.data):
             # same data on disk
-            return None
+            return
 
-        dirname = _os.path.dirname(abs_out_path)
-        try:
-            _os.makedirs(dirname, exist_ok = True)
-        except OSError as exc:
-            handle_ENAMETOOLONG(exc, dirname)
-            raise exc
-
-        def make_dst(dst_part : _t.AnyStr) -> None:
-            try:
-                with open(dst_part, "xb") as f:
-                    f.write(self.data)
-            except FileExistsError:
-                raise Failure(gettext(f"trying to overwrite `%s` which already exists"), dst_part)
-
-        try:
-            atomic_make_file(make_dst, abs_out_path, dsync, True)
-        except FileExistsError:
-            raise Failure(gettext(f"trying to overwrite `%s` which already exists"), abs_out_path)
-        except OSError as exc:
-            handle_ENAMETOOLONG(exc, dirname)
-            raise exc
-
-        return None
+        undeferred_write(self.data, abs_out_path, dsync, True)