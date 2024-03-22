Own-Data-Privateer/hoardy-web

commit 1ad0f2c7de4defe5539f10b3f73c5dc38a7bffdc

tool: store `Reqres` timestamps as `Decimal`s, `EpochMsec` -> `Epoch`
Evaluating `request.started_at`, `response.started_at`, `finished_at` will now
return time in **seconds** since UNIX epoch.
The `py` formatter will print them as prorper `Decimal`s, `json` and `cbor` will
dump them as `float`s.

tool/wrrarms/type.py
@@ -15,33 +15,21 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import decimal as _dec
 import time as _time
 import typing as _t
+
 from kisstdlib.exceptions import *
 
-WrappedValueType = _t.TypeVar("WrappedValueType")
-class WrappedValue(_t.Generic[WrappedValueType]):
-    def __init__(self, value : WrappedValueType) -> None:
-        self.value = value
-
-    def __repr__(self) -> str:
-        return "<%s %s>" % (self.__class__.__name__, repr(self.value))
-
-class EpochMsec(WrappedValue[int]):
+class Epoch(_dec.Decimal):
     def format(self, fmt : str = "%Y-%m-%d %H:%M:%S") -> str:
-        return _time.strftime(fmt, _time.localtime(self.value // 1000)) + "." + format(self.value % 1000, "03")
-
-    def __str__(self) -> str:
-        return self.format()
+        return _time.strftime(fmt, _time.localtime(int(self))) + "." + format(int(self * 1000) % 1000, "03")
 
     def __repr__(self) -> str:
-        return "<EpochMsec %s>" % (self.format(),)
+        return "<Epoch %s>" % (self.format(),)
 
-    def __int__(self) -> int:
-        return self.value
-
-def fmt_msec_diff(from_msec : EpochMsec, to_msec : EpochMsec) -> str:
-    value = to_msec.value - from_msec.value
+def fmt_epoch_diff(from_epoch : Epoch, to_epoch : Epoch) -> str:
+    value = int((to_epoch - from_epoch) * 1000)
     hours = value // 3600000
     value = value % 3600000
     minutes = value // 60000
@@ -50,8 +38,8 @@ def fmt_msec_diff(from_msec : EpochMsec, to_msec : EpochMsec) -> str:
     value = value % 1000
     return str(hours) + ":" + format(minutes, "02") + ":" + format(seconds, "02") + "." + format(value, "03")
 
-def fmt_msec_interval(from_msec : EpochMsec, to_msec : EpochMsec) -> str:
-    return f"[{str(from_msec)}]--[{str(to_msec)}] => {fmt_msec_diff(from_msec, to_msec)}"
+def fmt_epoch_interval(from_epoch : Epoch, to_epoch : Epoch) -> str:
+    return f"[{str(from_epoch)}]--[{str(to_epoch)}] => {fmt_epoch_diff(from_epoch, to_epoch)}"
 
 def rec_get(obj : _t.Any, field : list[str]) -> _t.Any:
     if len(field) == 0:
@@ -66,8 +54,8 @@ def rec_get(obj : _t.Any, field : list[str]) -> _t.Any:
         raise Failure("object of type `%s` does not have an attribute named `%s'", type(obj).__name__, this)
 
 def plainify(obj : _t.Any) -> _t.Any:
-    if isinstance(obj, WrappedValue):
-        return obj.value
+    if isinstance(obj, Epoch):
+        return float(obj)
     elif hasattr(obj, "__dataclass_fields__"):
         res = dict()
         for k in obj.__dataclass_fields__:

tool/wrrarms/__main__.py
@@ -85,10 +85,8 @@ def load_wrr(path : str) -> ReqresExpr:
 def get_bytes(expr : str, rrexpr : ReqresExpr) -> bytes:
     value = rrexpr.eval(expr)
 
-    if value is None or isinstance(value, (bool, int, float)):
+    if value is None or isinstance(value, (bool, int, float, Epoch)):
         value = str(value)
-    elif isinstance(value, EpochMsec):
-        value = str(int(value))
 
     if isinstance(value, str):
         return value.encode(_sys.getdefaultencoding())

tool/wrrarms/output.py
@@ -71,7 +71,7 @@ def wrr_pprint(fobj : TIOWrappedWriter, reqres : Reqres, path : str | bytes, abr
     else:
         fobj.write_str_ln("response none")
 
-    fobj.write_str_ln(f"clock {fmt_msec_interval(req.started_at, reqres.finished_at)}")
+    fobj.write_str_ln(f"clock {fmt_epoch_interval(req.started_at, reqres.finished_at)}")
 
     if len(reqres.extra) > 0:
         for k, v in reqres.extra.items():
@@ -259,9 +259,9 @@ class PyStreamEncoder(StreamEncoder):
 
     @staticmethod
     def encode_py(enc : PyReprEncoder, obj : _t.Any) -> None:
-        if isinstance(obj, EpochMsec):
-            enc.lexeme(str(int(obj)))
-            enc.comment(str(obj))
+        if isinstance(obj, Epoch):
+            enc.lexeme(str(obj))
+            enc.comment(obj.format())
         else:
             enc.encode(plainify(obj))
 
@@ -352,8 +352,8 @@ class RawStreamEncoder(StreamEncoder):
 
     @staticmethod
     def encode_raw(enc : TIOEncoder, obj : _t.Any) -> None:
-        if isinstance(obj, EpochMsec):
-            enc.write_str(str(int(obj)))
+        if isinstance(obj, Epoch):
+            enc.write_str(str(obj))
         else:
             raise Failure("can't raw-encode a value of type `%s`", type(obj).__name__)
 

tool/wrrarms/wrr.py
@@ -17,6 +17,7 @@
 
 import cbor2 as _cbor2
 import dataclasses as _dc
+import decimal as _dec
 import gzip as _gzip
 import hashlib as _hashlib
 import io as _io
@@ -37,7 +38,7 @@ Headers = list[tuple[str, bytes]]
 
 @_dc.dataclass
 class Request:
-    started_at : EpochMsec
+    started_at : Epoch
     method : str
     url : str
     headers : Headers
@@ -46,7 +47,7 @@ class Request:
 
 @_dc.dataclass
 class Response:
-    started_at : EpochMsec
+    started_at : Epoch
     code : int
     reason : str
     headers : Headers
@@ -60,33 +61,33 @@ class Reqres:
     protocol : str
     request : Request
     response : _t.Optional[Response]
-    finished_at : EpochMsec
+    finished_at : Epoch
     extra : dict[str, _t.Any]
 
 Reqres_fields = {
     "version": "WEBREQRES format version; int",
     "source": "`+`-separated list of applications that produced this reqres; str",
     "protocol": 'protocol (e.g. `"HTTP/1.0"`, `"HTTP/2.0"`); str',
-    "request.started_at": "request start time in milliseconds since 1970-01-01 00:00; EpochMsec",
+    "request.started_at": "request start time in seconds since 1970-01-01 00:00; Epoch",
     "request.method": 'request HTTP method (`"GET"`, `"POST"`, etc); str',
     "request.url": "request URL, including the fragment/hash part; str",
     "request.headers": "request headers; list[tuple[str, bytes]]",
     "request.complete": "is request body complete?; bool",
     "request.body": "request body; bytes",
-    "response.started_at": "response start time in milliseconds since 1970-01-01 00:00; EpochMsec",
+    "response.started_at": "response start time in seconds since 1970-01-01 00:00; Epoch",
     "response.code": "HTTP response code (like `200`, `404`, etc); int",
     "response.reason": 'HTTP response reason (like `"OK"`, `"Not Found"`, etc); usually empty for Chromium and filled for Firefox; str',
     "response.headers": "response headers; list[tuple[str, bytes]]",
     "response.complete": "is response body complete?; bool",
     "response.body": "response body; Firefox gives raw bytes, Chromium gives UTF-8 encoded strings; bytes | str",
-    "finished_at": "request completion time in milliseconds since 1970-01-01 00:00; EpochMsec"
+    "finished_at": "request completion time in seconds since 1970-01-01 00:00; Epoch",
 }
 
 Reqres_derived_attrs = {
     "fs_path": "file system path for the WRR file containing this reqres; str",
 
-    "qtime_ms": 'aliast for `request.started_at`; mnemonic: "reQuest TIME"; int',
-    "qtime": "`qtime_ms` rounded down to seconds (UNIX epoch); int",
+    "qtime": 'aliast for `request.started_at`; mnemonic: "reQuest TIME"; seconds since UNIX epoch; decimal float',
+    "qtime_ms": "`qtime` in milliseconds rounded down to nearest integer; milliseconds since UNIX epoch; int",
     "qtime_msq": "three least significant digits of `qtime_ms`; int",
     "qyear": "year number of `gmtime(qtime)` (UTC year number of `qtime`); int",
     "qmonth": "month number of `gmtime(qtime)`; int",
@@ -95,8 +96,8 @@ Reqres_derived_attrs = {
     "qminute": "minute of `gmtime(qtime)`; int",
     "qsecond": "second of `gmtime(qtime)`; int",
 
-    "stime_ms": '`response.started_at` if there was a response, `finished_at` otherwise; mnemonic: "reSponse TIME"; int',
-    "stime": "`stime_ms` rounded down to seconds (UNIX epoch); int",
+    "stime": '`response.started_at` if there was a response, `finished_at` otherwise; mnemonic: "reSponse TIME"; seconds since UNIX epoch; decimal float',
+    "stime_ms": "`stime` in milliseconds rounded down to nearest integer; milliseconds since UNIX epoch, int",
     "stime_msq": "three least significant digits of `stime_msq`; int",
     "syear": "similar to `syear`, but for `stime`; int",
     "smonth": "similar to `smonth`, but for `stime`; int",
@@ -105,8 +106,8 @@ Reqres_derived_attrs = {
     "sminute": "similar to `sminute`, but for `stime`; int",
     "ssecond": "similar to `ssecond`, but for `stime`; int",
 
-    "ftime_ms": "aliast for `finished_at`; int",
-    "ftime": "`ftime_ms` rounded down to seconds (UNIX epoch); int",
+    "ftime": "aliast for `finished_at`; seconds since UNIX epoch; decimal float",
+    "ftime_ms": "`ftime` in milliseconds rounded down to nearest integer; milliseconds since UNIX epoch; int",
     "ftime_msq": "three least significant digits of `ftime_msq`; int",
     "fyear": "similar to `syear`, but for `ftime`; int",
     "fmonth": "similar to `smonth`, but for `ftime`; int",
@@ -147,8 +148,8 @@ class ReqresExpr:
             "fs_path": path
         }
 
-    def _fill_time(self, prefix : str, ts : int) -> None:
-        dt = _time.gmtime(ts)
+    def _fill_time(self, prefix : str, ts : Epoch) -> None:
+        dt = _time.gmtime(int(ts))
         self.items[prefix + "year"] = dt.tm_year
         self.items[prefix + "month"] = dt.tm_mon
         self.items[prefix + "day"] = dt.tm_mday
@@ -170,37 +171,37 @@ class ReqresExpr:
             self.items[name] = reqres.request.method
         elif (name.startswith("q") and \
               name[1:] in _time_attrs):
-            qtime_ms = int(reqres.request.started_at)
-            qtime = qtime_ms // 1000
-            self.items["qtime_ms"] = qtime_ms
+            qtime = reqres.request.started_at
+            qtime_ms = int(qtime * 1000)
             self.items["qtime"] = qtime
+            self.items["qtime_ms"] = qtime_ms
             self.items["qtime_msq"] = qtime_ms % 1000
             self._fill_time("q", qtime)
         elif (name.startswith("s") and \
               name[1:] in _time_attrs) or \
               name == "status":
             if reqres.response is not None:
-                stime_ms = int(reqres.response.started_at)
+                stime = reqres.response.started_at
                 status = str(reqres.response.code)
                 if reqres.response.complete:
                     status += "C"
                 else:
                     status += "N"
             else:
-                stime_ms = int(reqres.finished_at)
+                stime = reqres.finished_at
                 status = "NR"
+            stime_ms = int(stime * 1000)
             self.items["status"] = status
-            stime = stime_ms // 1000
-            self.items["stime_ms"] = stime_ms
             self.items["stime"] = stime
+            self.items["stime_ms"] = stime_ms
             self.items["stime_msq"] = stime_ms % 1000
             self._fill_time("s", stime)
         elif (name.startswith("f") and \
               name[1:] in _time_attrs):
-            ftime_ms = int(reqres.finished_at)
-            ftime = ftime_ms // 1000
-            self.items["ftime_ms"] = ftime_ms
+            ftime = reqres.finished_at
+            ftime_ms = int(ftime * 1000)
             self.items["ftime"] = ftime
+            self.items["ftime_ms"] = ftime_ms
             self.items["ftime_msq"] = ftime_ms % 1000
             self._fill_time("f", ftime)
         elif name in ["full_url", "net_url",
@@ -304,8 +305,11 @@ def _t_int(x : _t.Any) -> int:
     if isinstance(x, int): return x
     raise TypeError("wrong type: want %s, got %s", int, type(x))
 
-def _t_epoch(x : _t.Any) -> EpochMsec:
-    return EpochMsec(_t_int(x))
+def _t_epoch(x : _t.Any) -> Epoch:
+    return Epoch(_dec.Decimal(_t_int(x)) / 1000)
+
+def _f_epoch(x : Epoch) -> int:
+    return int(x * 1000)
 
 def _t_headers(x : _t.Any) -> Headers:
     if Headers.__instancecheck__(x):
@@ -345,17 +349,17 @@ def wrr_loadf(path : str | bytes) -> Reqres:
 
 def wrr_dumps(reqres : Reqres, compress : bool = True) -> bytes:
     req = reqres.request
-    request = req.started_at.value, req.method, req.url, req.headers, req.complete, req.body
+    request = _f_epoch(req.started_at), req.method, req.url, req.headers, req.complete, req.body
     del req
 
     if reqres.response is None:
         response = None
     else:
         res = reqres.response
-        response = res.started_at.value, res.code, res.reason, res.headers, res.complete, res.body
+        response = _f_epoch(res.started_at), res.code, res.reason, res.headers, res.complete, res.body
         del res
 
-    structure = ["WEBREQRES/1", reqres.source, reqres.protocol, request, response, reqres.finished_at.value, reqres.extra]
+    structure = ["WEBREQRES/1", reqres.source, reqres.protocol, request, response, _f_epoch(reqres.finished_at), reqres.extra]
 
     data : bytes = _cbor2.dumps(structure)
 

commit 3cd99ca18284b493eea5673177a2e8e25ee5641f

tool: pprint: fix `clock` line output format
Previosly broken by 1ad0f2c7de4defe5539f10b3f73c5dc38a7bffdc.

tool/wrrarms/type.py
@@ -39,7 +39,7 @@ def fmt_epoch_diff(from_epoch : Epoch, to_epoch : Epoch) -> str:
     return str(hours) + ":" + format(minutes, "02") + ":" + format(seconds, "02") + "." + format(value, "03")
 
 def fmt_epoch_interval(from_epoch : Epoch, to_epoch : Epoch) -> str:
-    return f"[{str(from_epoch)}]--[{str(to_epoch)}] => {fmt_epoch_diff(from_epoch, to_epoch)}"
+    return f"[{from_epoch.format()}]--[{to_epoch.format()}] => {fmt_epoch_diff(from_epoch, to_epoch)}"
 
 def rec_get(obj : _t.Any, field : list[str]) -> _t.Any:
     if len(field) == 0: