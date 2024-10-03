c9045fe10d9178b80724facdca71dd3111159f70
Jan Malakhovski <oxij@oxij.org>
2024-10-03 12:00:00 +0000

    simple_server/*, CHANGELOG.md: bump version

--- simple_server/default.nix
+++ simple_server/default.nix
@@ -7,7 +7,7 @@ with pkgs;
 
 python3Packages.buildPythonApplication rec {
   pname = "hoardy-web-sas";
-  version = "1.6.1";
+  version = "1.7.0";
   format = "pyproject";
 
   inherit (source) src unpackPhase;
