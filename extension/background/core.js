Own-Data-Privateer/hoardy-web

commit 00e0f01d45d2f625e1b995920f86bcf78377c9c0

extension/background/core: stop leaking extension's id into the dumps

extension/background/core.js
@@ -3268,12 +3268,16 @@ function handleBeforeRequest(e) {
         fromCache: false,
     };
 
-    if (isDefinedURL(e.documentUrl))
+    if (isDefinedURL(e.documentUrl)
+        && !e.documentUrl.startsWith(selfURL)) // just in case
         reqres.documentUrl = e.documentUrl;
 
-    if (isDefinedURL(e.originUrl))
+    if (isDefinedURL(e.originUrl)
+        && !e.originUrl.startsWith(selfURL)) // do not leak extension id when using config.workaroundFirefoxFirstRequest
         reqres.originUrl = e.originUrl; // Firefox
-    else if (isDefinedURL(e.initiator) && e.initiator !== "null")
+    else if (isDefinedURL(e.initiator)
+             && e.initiator !== "null"
+             && !e.originUrl.startsWith(selfURL)) // just in case
         reqres.originUrl = e.initiator; // Chromium
 
     if (e.requestBody !== undefined && e.requestBody !== null) {

commit a69fa68d749c348813c78305b0fbbc8741f0025e

extension: fix request tracking being frequently broken on Chromium
Introduced by 00e0f01d45d2f625e1b995920f86bcf78377c9c0.

extension/background/core.js
@@ -3244,7 +3244,7 @@ function handleBeforeRequest(e) {
         reqres.originUrl = e.originUrl; // Firefox
     else if (isDefinedURL(e.initiator)
              && e.initiator !== "null"
-             && !e.originUrl.startsWith(selfURL)) // just in case
+             && !e.initiator.startsWith(selfURL)) // just in case
         reqres.originUrl = e.initiator; // Chromium
 
     if (e.requestBody !== undefined && e.requestBody !== null) {