Own-Data-Privateer/hoardy-web

commit 2a201498f7a00b9840167f5e358b43144eb84c1e

extension/{background/core, page/{popup, help}}: rename per-source option `profile` -> `bucket`, improve its documentation

extension/background/core.js
@@ -119,21 +119,21 @@ let configDefaults = {
         collecting: true,
         limbo: false,
         negLimbo: false,
-        profile: "default",
+        bucket: "default",
     },
 
     extension: {
         collecting: false,
         limbo: false,
         negLimbo: false,
-        profile: "extension",
+        bucket: "extension",
     },
 
     background: {
         collecting: true,
         limbo: false,
         negLimbo: false,
-        profile: "background",
+        bucket: "background",
     },
 };
 // current config
@@ -1427,8 +1427,7 @@ async function processOneArchiving(archivable) {
     if (config.discardAll)
         return;
 
-    let options = getOriginConfig(loggable.tabId, loggable.fromExtension);
-    let archiveURL = config.submitHTTPURLBase + "?profile=" + encodeURIComponent(options.profile);
+    let archiveURL = config.submitHTTPURLBase + "?profile=" + encodeURIComponent(loggable.bucket || config.root.bucket);
 
     let byErrorMap = reqresFailedToSubmit.get(archiveURL);
     if (byErrorMap !== undefined) {
@@ -1487,6 +1486,7 @@ async function processArchiving() {
         let [loggable, dump] = archivable;
         try {
             reqresQueueSize -= loggable.dumpSize;
+            updateLoggable(loggable);
             await processOneArchiving(archivable);
             await updateDisplay(true, loggable.tabId, 10);
         } catch (err) {
@@ -1763,10 +1763,11 @@ async function processOneAlmostDone(reqres, newProblematic, newLimbo, newQueued,
                     "res", reqres.responseComplete,
                     "result", statusCode, reqres.reason, reqres.statusLine,
                     "errors", reqres.errors,
-                    "profile", options.profile,
+                    "bucket", options.bucket,
                     reqres);
 
     let loggable = makeLoggableReqres(reqres);
+    loggable.bucket = options.bucket;
     loggable.net_state = state;
     loggable.was_problematic = loggable.problematic = problematic;
     loggable.picked = picked;
@@ -2198,6 +2199,14 @@ function makeLoggableReqres(reqres) {
     let loggable = shallowCopyOfReqres(reqres);
     addLoggableFields(loggable);
     return loggable;
+}
+
+function updateLoggable(loggable) {
+    if (loggable.sessionId !== sessionId)
+        return;
+
+    let options = getOriginConfig(loggable.tabId, loggable.fromExtension);
+    loggable.bucket = options.bucket;
 }
 
 // handlers
@@ -2936,6 +2945,10 @@ function upgradeConfig(cfg) {
         rename("archiveNotifyOK", "archiveDoneNotify")
         rename("archiveNotifyFailed", "archiveFailedNotify")
         rename("archiveNotifyDisabled", "archiveStuckNotify")
+
+        config.root.bucket = config.root.profile;
+        config.extension.bucket = config.extension.profile;
+        config.background.bucket = config.background.profile;
     case 5:
         break;
     default:

extension/page/help.org
@@ -158,15 +158,25 @@ Note, however, that /problematic reqres/ will not get automatically elided from 
 To forget about them, you will have to unset the /problematic/ flag on the respective reqres via [[./popup.html#unmarkAllTabProblematic][this button]], or [[./popup.html#unmarkAllProblematic][that button]], or use similar buttons in the [[./popup.html#showState][log]].
 ** Step 4: Archival
 When [[./popup.html#div-config.archive]["Archive collected reqres" toggle]] is enabled, pWebArc will pop =collected= reqres from the archival queue one by one, serialize them into CBOR-formatted dumps, and then push those dumps to the archiving server at [[./popup.html#div-config.submitHTTPURLBase]["Archive collected reqres to URL" setting]] by turning each reqres into a POST HTTP request with the dump of the reqres as request body (which is denoted by =srvIO= state on the diagram).
-It will also specify =profile= query parameter to the POST request using the appropriate "Profile" setting, e.g.
+*** Buckets
+:PROPERTIES:
+:CUSTOM_ID: bucket
+:END:
 
-- [[./popup.html#div-tabconfig.profile][this one]] will be used for requests originating from the currently active tab,
-- [[./popup.html#div-tabconfig.children.profile][this one]] will be used for requests originating from new child tabs opened from the currently active tab (e.g. via middle mouse click, context menu, etc),
-- while [[./popup.html#div-config.root.profile][this one]] will be used for new tabs opened via browser's "New Tab" browser action (i.e. the plus sign in the tab bar, =Control+T=, menu item, etc),
+Sometimes you might want to split your archivals into separate /buckets/ to simplify future hoarding and sharing of collected archives.
+E.g., say, by default you might want to put everything into the "default" bucket, but then you might want to put reqres produced by a select tab where you just logged in into you personal account into the "private" bucket instead.
+
+To implement this, for each reqres in the archival queue, pWebArc computes a =bucket= parameter from the appropriate "Bucket" setting, e.g.
+
+- [[./popup.html#div-tabconfig.bucket][this one]] will be used for requests originating from the currently active tab,
+- [[./popup.html#div-tabconfig.children.bucket][this one]] will be used for requests originating from new child tabs opened from the currently active tab (e.g. via middle mouse click, context menu, etc),
+- while [[./popup.html#div-config.root.bucket][this one]] will be used for new tabs opened via browser's "New Tab" browser action (i.e. the plus sign in the tab bar, =Control+T=, menu item, etc),
 - and so forth for the others (press "?" symbols to see a tooltip explaining what each of them does).
 
-Evaluation of the =profile=  parameter is done just before the POST request is sent, so if the queue is not yet empty, and you disable [[./popup.html#div-config.archive]["Archive collected reqres"]], edit some of the "Profile" settings, and enable [[./popup.html#div-config.archive][it]] again, pWebArc will start using the new setting immediately.
+Evaluation of the =bucket= parameter is done just before each archival attempt, so if the queue is not yet empty, and you disable [[./popup.html#div-config.archive]["Archive =collected= reqres"]], edit some of the "Bucket" settings, and enable [[./popup.html#div-config.archive][it]] again, pWebArc will start using the new setting immediately.
 
+When submitting to an HTTP server, pWebArc will specify =bucket= as a query parameter (named "profile", for historical reasons) to each HTTP POST request.
+*** Failures
 If [[./popup.html#div-config.archiveFailedNotify][this option]] is enabled and some reqres failed to be archived, a new desktop notification will be generated.
 If [[./popup.html#div-config.archiveDoneNotify][this option]] is enabled, a new desktop notification will be generated when the archival queue gets empty the very first time or after any failures.
 *** Glossary

extension/page/popup.template
@@ -242,7 +242,7 @@ label code {
 
     <div name="more" class="opts">
     <ui id="config.collecting" type="boolean" class="prefer-on" data-help="Also known as &quot;Enable this add-on&quot;. Generate a new reqres and start tracking its progress when the browser makes a new HTTP request. Disabling this option will stop pWebArc from recording any new data. Already tracked reqres will still get tracked until they finish.">Track newly generated requests</ui>
-    <ui id="config.archive" type="boolean" class="prefer-on" data-help="What it says on the tin. Disabling this will keep them stuck in the archiving queue indefinitely, in which case restarting the browser or reloading the extension will loose them. Which is why you probably shouldn&#039;t disable this option, unless you want to disable this, atomically change some of the &quot;Profile&quot; settings below, and then enable this again.">Archive <code>collected</code> reqres</ui>
+    <ui id="config.archive" type="boolean" class="prefer-on" data-help="What it says on the tin. Disabling this will keep them stuck in the archiving queue indefinitely, in which case restarting the browser or reloading the extension will loose them. Which is why you probably shouldn&#039;t disable this option, unless you want to disable this, atomically change some of the &quot;Bucket&quot; settings below, and then enable this again.">Archive <code>collected</code> reqres</ui>
     <ui id="config.submitHTTPURLBase" type="string" class="da ds" data-help="URL of the server we are archiving to.">... at URL</ui>
     </div>
 
@@ -327,7 +327,7 @@ label code {
 
     <div name="more" class="opts">
     <ui id="tabconfig.negLimbo" type="boolean" class="prefer-off" data-help="For this tab, put newly `finished` reqres that do NOT satisfy &quot;Pick reqres for archival when they finish&quot; settings into limbo instead of discarding them immediately. Useful for debugging.">Drop into limbo</ui>
-    <ui id="tabconfig.profile" type="string" class="da" data-help="Use this server-side profile when archiving reqres originating from this tab.">Profile</ui>
+    <ui id="tabconfig.bucket" type="string" class="da" data-help="Use this bucket when archiving reqres originating from this tab.">Bucket</ui>
     </div>
     <!-- thisTabOptions -->
     </div>
@@ -342,7 +342,7 @@ label code {
 
     <div name="more" class="opts">
     <ui id="tabconfig.children.negLimbo" type="boolean" class="prefer-off" data-help="Enable &quot;Drop into limbo&quot; option in new tabs spawned from this tab (via middle mouse click, context menu actions).">Enable &quot;Drop into limbo&quot;</ui>
-    <ui id="tabconfig.children.profile" type="string" class="da" data-help="Use this server-side profile when archiving reqres originating from new tabs opened from this tab (middle mouse click, context menu actions).">Profile</ui>
+    <ui id="tabconfig.children.bucket" type="string" class="da" data-help="Use this bucket when archiving reqres originating from new tabs opened from this tab (middle mouse click, context menu actions).">Bucket</ui>
     </div>
     <!-- thisTabChildrenOptions -->
     </div>
@@ -358,7 +358,7 @@ label code {
     <!-- smallMore -->
     <div class="opts">
     <ui id="config.root.negLimbo" type="boolean" class="prefer-off" data-help="Enable &quot;Drop into limbo&quot; option in new tabs opened via browser&#039;s &quot;New Tab&quot; action (new tabs created via the plus sign in the tab bar, Control+T, &quot;New Tab&quot; menu item). This setting also gets applied to all open tabs when pWebArc is (re)loaded.">Enable &quot;Drop into limbo&quot;</ui>
-    <ui id="config.root.profile" type="string" class="da" data-help="Use this server-side profile when archiving reqres originating from new tabs opened via browser&#039;s &quot;New Tab&quot; browser action (new tabs created via the plus sign in the tab bar, Control+T, &quot;New Tab&quot; menu item). This setting also gets applied to all open tabs when pWebArc is (re)loaded.">Profile</ui>
+    <ui id="config.root.bucket" type="string" class="da" data-help="Use this bucket when archiving reqres originating from new tabs opened via browser&#039;s &quot;New Tab&quot; browser action (new tabs created via the plus sign in the tab bar, Control+T, &quot;New Tab&quot; menu item). This setting also gets applied to all open tabs when pWebArc is (re)loaded.">Bucket</ui>
     </div>
     <!-- smallMore -->
     </div>
@@ -375,7 +375,7 @@ label code {
     <ui id="config.background.collecting" type="boolean" class="prefer-on dc" data-help="Track new reqres originating from browser&#039;s background tasks (excluding those originating from extensions). With the default browser config you probably should not disable this as browsers do link pre-fetching in background, and disabling this will miss that data.">Track new reqres</ui>
     <ui id="config.background.limbo" type="boolean" data-help="For reqres originating from browser&#039;s background tasks (excluding those originating from extensions), put newly `finished` reqres tab that satisfy &quot;Pick reqres for archival when they finish&quot; settings into limbo until you collect them for archival or discard them manually.">Pick into limbo</ui>
     <ui id="config.background.negLimbo" type="boolean" class="prefer-off" data-help="For reqres originating from browser&#039;s background tasks (excluding those originating from extensions), put newly `finished` reqres tab that do NOT satisfy &quot;Pick reqres for archival when they finish&quot; settings into limbo instead of discarding them immediately. Useful for debugging.">Drop into limbo</ui>
-    <ui id="config.background.profile" type="string" class="da" data-help="Use this server-side profile when archiving reqres originating from browser&#039;s background tasks (excluding those originating from extensions).">Profile</ui>
+    <ui id="config.background.bucket" type="string" class="da" data-help="Use this bucket when archiving reqres originating from browser&#039;s background tasks (excluding those originating from extensions).">Bucket</ui>
     </div>
     <!-- backgroundOptions -->
     </div>
@@ -387,7 +387,7 @@ label code {
     <ui id="config.extension.collecting" type="boolean" class="dc" data-help="Track new reqres originating from browser extensions.">Track new reqres</ui>
     <ui id="config.extension.limbo" type="boolean" data-help="For reqres originating from browser&#039;s extensions/add-ons, put newly `finished` reqres tab that satisfy &quot;Pick reqres for archival when they finish&quot; settings into limbo until you collect them for archival or discard them manually.">Pick into limbo</ui>
     <ui id="config.extension.negLimbo" type="boolean" class="prefer-off" data-help="For reqres originating from browser&#039;s extensions/add-ons, put newly `finished` reqres tab that do NOT satisfy &quot;Pick reqres for archival when they finish&quot; settings into limbo instead of discarding them immediately. Useful for debugging.">Drop into limbo</ui>
-    <ui id="config.extension.profile" type="string" class="da" data-help="Use this server-side profile when archiving reqres originating from browser extensions.">Profile</ui>
+    <ui id="config.extension.bucket" type="string" class="da" data-help="Use this bucket when archiving reqres originating from browser extensions.">Bucket</ui>
     </div>
     <!-- extensionOptions -->
     </div>

commit 9594be5b2c99a852f1774081f2662110541bfe9e

extension/background/core.js: fix a bug in `upgradeConfig`
Introduced in 2a201498f7a00b9840167f5e358b43144eb84c1e.

extension/background/core.js
@@ -4018,9 +4018,9 @@ function upgradeConfig(cfg) {
         rename("archiveNotifyFailed", "archiveFailedNotify")
         rename("archiveNotifyDisabled", "archiveStuckNotify")
 
-        config.root.bucket = config.root.profile;
-        config.extension.bucket = config.extension.profile;
-        config.background.bucket = config.background.profile;
+        cfg.root.bucket = cfg.root.profile;
+        cfg.extension.bucket = cfg.extension.profile;
+        cfg.background.bucket = cfg.background.profile;
     case 5:
         break;
     default: