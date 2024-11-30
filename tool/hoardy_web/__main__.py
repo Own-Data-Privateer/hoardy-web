Own-Data-Privateer/hoardy-web

commit 37c06af51212d89ab2c8446e8b1ab07a4f7e034d

tool/hoardy_web/*: rework `ReqresExpr` source handling, implement lazy `Reqres` loading, unloading, and re-loading
- Rework `DeferredSource` class.
- Make `ReqresExpr` take a type argument with a subtype of `DeferredSource`.
- Make `ReqresExpr` take its `DeferredSource` as the first argument of its
  constructor.
- Make `ReqresExpr` itself into a subtype of `DeferredSource`.
- Implement `unload` for `ReqresExpr`, allowing the underlying `Reqres`
  to be unloaded and re-loaded on demand.
- Rework everything else to conform to this.

I.e. this makes `ReqresExpr` optionally lazy and un-loadable with respect to
parsing of underlying `Reqres` structures.

Important side-effects of this are:

- `organize` can now take `WRR` bundles as inputs,
- `export mirror` becomes much faster at indexing inputs that contain archives
  of the same URLs, repeatedly.

tool/hoardy_web/wrr.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import abc as _abc
 import cbor2 as _cbor2
 import dataclasses as _dc
 import gzip as _gzip
@@ -33,9 +34,10 @@ from kisstdlib.exceptions import *
 from kisstdlib.io.stdio import *
 from kisstdlib.path import *
 
-from .util import *
-from .type import *
 from .linst import *
+from .source import *
+from .type import *
+from .util import *
 from .web import *
 
 class RRCommon:
@@ -73,14 +75,14 @@ class Request(RRCommon):
     complete : bool
     body : bytes | str
 
+    def approx_size(self) -> int:
+        return 56 + 2 * len(self.url.raw_url) + len(self.body) \
+            + sum(map(lambda x: len(x[0]) + len(x[1]), self.headers))
+
     def get_content_type(self) -> tuple[str, bool]:
         ct = self.get_header("content-type", "application/x-www-form-urlencoded")
         assert ct is not None
         return ct, False
-
-    def approx_size(self) -> int:
-        return 64 + 2 * len(self.url.raw_url) + len(self.body) \
-            + sum(map(lambda x: len(x[0]) + len(x[1]), self.headers))
 
 @_dc.dataclass
 class Response(RRCommon):
@@ -90,6 +92,10 @@ class Response(RRCommon):
     headers : Headers
     complete : bool
     body : bytes | str
+
+    def approx_size(self) -> int:
+        return 56 + len(self.body) \
+            + sum(map(lambda x: len(x[0]) + len(x[1]), self.headers))
 
     def get_content_type(self) -> tuple[str, bool]:
         ct = self.get_header("content-type", "application/octet-stream")
@@ -101,10 +107,6 @@ class Response(RRCommon):
             sniff = True
         return ct, sniff
 
-    def approx_size(self) -> int:
-        return 64 + len(self.body) \
-            + sum(map(lambda x: len(x[0]) + len(x[1]), self.headers))
-
 @_dc.dataclass
 class WebSocketFrame:
     sent_at : Epoch
@@ -113,7 +115,7 @@ class WebSocketFrame:
     content : bytes
 
     def approx_size(self) -> int:
-        return 32 + len(self.content)
+        return 40 + len(self.content)
 
 Reqres_fields = {
     "version": "WEBREQRES format version; int",
@@ -507,34 +509,60 @@ ReqresExpr_lookup = linst_custom_or_env(ReqresExpr_atoms)
 ReqresExpr_time_attrs = frozenset(["time", "time_ms", "time_msq", "year", "month", "day", "hour", "minute", "second"])
 
 @_dc.dataclass
-class ReqresExpr:
-    reqres : Reqres
-    fs_path : str | bytes | None
-    obj_path : list[int | str | bytes]
-    in_stat : _os.stat_result | None = _dc.field(default = None)
+class ReqresExpr(DeferredSource, _t.Generic[DeferredSourceType]):
+    source : DeferredSourceType
+
+    _reqres : Reqres | None
+    original : _t.Any | None = _dc.field(default = None)
+
     sniff : SniffContentType = _dc.field(default=SniffContentType.NONE)
     remap_url : URLRemapper | None = _dc.field(default = None)
+
     items : dict[str, _t.Any] = _dc.field(default_factory = dict)
 
-    def format_source(self) -> bytes:
-        res = _io.BytesIO()
-        fs_path = self.fs_path
-        if fs_path is None:
-            res.write(_os.fsencode(f"<{id(self)}>"))
-        elif isinstance(fs_path, str):
-            res.write(_os.fsencode(fs_path))
-        else:
-            res.write(fs_path)
+    @property
+    def reqres(self) -> Reqres:
+        reqres = self._reqres
+        if reqres is not None:
+            return reqres
 
-        for o in self.obj_path:
-            res.write(b"/")
-            if isinstance(o, int):
-                res.write(_os.fsencode(str(o)))
-            elif isinstance(o, str):
-                res.write(_os.fsencode(o))
-            else:
-                res.write(o)
-        return res.getvalue()
+        source = self.source
+        if isinstance(self.source, FileSource):
+            reqres = wrr_load(source.get_fileobj())
+        else:
+            raise NotImplementedError()
+
+        self._reqres = reqres
+        return reqres
+
+    def unload(self, completely : bool = False) -> None:
+        if isinstance(self.source, FileSource):
+            # this `reqres` is cheap to re-load
+            self._reqres = None
+        if completely:
+            self.items = dict()
+
+    def approx_size(self) -> int:
+        return 128 + \
+            (self.source.approx_size() if self.source is not None else 0) + \
+            (self._reqres.approx_size() if self._reqres is not None else 0) + \
+            sum(map(lambda k: len(k) + 16, self.items.keys()))
+
+    def show_source(self) -> str:
+        return self.source.show_source()
+
+    def get_fileobj(self) -> _io.BufferedReader:
+        return BytesIOReader(wrr_dumps(self.reqres))
+
+    def same_as(self, other : DeferredSource) -> bool:
+        if isinstance(other, ReqresExpr):
+            return self.source.same_as(other.source)
+        return self.source.same_as(other)
+
+    def replaces(self, other : DeferredSource) -> bool:
+        if isinstance(other, ReqresExpr):
+            return self.source.replaces(other.source)
+        return self.source.replaces(other)
 
     def _fill_time(self, prefix : str, ts : Epoch) -> None:
         dt = _time.gmtime(int(ts))
@@ -555,7 +583,10 @@ class ReqresExpr:
             pass
 
         if name == "fs_path":
-            return self.fs_path
+            if isinstance(self.source, FileSource):
+                return self.source.path
+            else:
+                return None
 
         reqres = self.reqres
         if name == "method":
@@ -646,25 +677,22 @@ class ReqresExpr:
             raise Failure("expression `%s` evaluated to `None`", expr)
         return res
 
-def rrexpr_wrr_load(fobj : _io.BufferedReader, path : str | bytes) -> ReqresExpr:
-    return ReqresExpr(wrr_load(fobj), path, [])
+def rrexpr_wrr_load(fobj : _io.BufferedReader, source : DeferredSourceType) -> ReqresExpr[DeferredSourceType]:
+    return ReqresExpr(source, wrr_load(fobj))
 
-def rrexprs_wrr_bundle_load(fobj : _io.BufferedReader, path : str | bytes) -> _t.Iterator[ReqresExpr]:
+def rrexprs_wrr_bundle_load(fobj : _io.BufferedReader, source : DeferredSourceType) -> _t.Iterator[ReqresExpr[StreamElementSource[DeferredSourceType]]]:
     n = 0
     for reqres in wrr_bundle_load(fobj):
-        yield ReqresExpr(reqres, path, [n])
+        yield ReqresExpr(StreamElementSource(source, n), reqres)
         n += 1
 
-def rrexpr_wrr_loadf(path : str | bytes) -> ReqresExpr:
+def rrexpr_wrr_loadf(path : str | bytes) -> ReqresExpr[FileSource]:
     with open(path, "rb") as f:
-        res = rrexpr_wrr_load(f, path)
-        res.in_stat = _os.fstat(f.fileno())
-        return res
+        return rrexpr_wrr_load(f, make_FileSource(path, f))
 
-def rrexprs_wrr_bundle_loadf(path : str | bytes) -> _t.Iterator[ReqresExpr]:
+def rrexprs_wrr_bundle_loadf(path : str | bytes) -> _t.Iterator[ReqresExpr[StreamElementSource[FileSource]]]:
     with open(path, "rb") as f:
-        yield from rrexprs_wrr_bundle_load(f, path)
-
+        yield from rrexprs_wrr_bundle_load(f, make_FileSource(path, f))
 def trivial_Reqres(url : ParsedURL,
                    content_type : str = "text/html",
                    qtime : Epoch = Epoch(0),
@@ -709,16 +737,16 @@ def fallback_Reqres(url : ParsedURL,
     # fallback this otherwise
     return trivial_Reqres(url, "application/octet-stream", stime, stime, stime, headers=headers, data=data)
 
-def mk_trivial_ReqresExpr(url : str, ct : str = "text/html", sniff : bool = False, headers : Headers = [], data : bytes = b"") -> ReqresExpr:
+def mk_trivial_ReqresExpr(url : str, ct : str = "text/html", sniff : bool = False, headers : Headers = [], data : bytes = b"") -> ReqresExpr[UnknownSource]:
     x = trivial_Reqres(parse_url(url), ct, sniff=sniff, headers=headers, data=data)
-    return ReqresExpr(x, None, [])
+    return ReqresExpr(UnknownSource(), x)
 
-def mk_fallback_ReqresExpr(url : str, cts : list[str] = ["text/html"], headers : Headers = [], data : bytes = b"") -> ReqresExpr:
+def mk_fallback_ReqresExpr(url : str, cts : list[str] = ["text/html"], headers : Headers = [], data : bytes = b"") -> ReqresExpr[UnknownSource]:
     x = fallback_Reqres(parse_url(url), cts, headers=headers, data=data)
-    return ReqresExpr(x, None, [])
+    return ReqresExpr(UnknownSource(), x)
 
 def test_ReqresExpr_url_parts() -> None:
-    def check(x : ReqresExpr, name : str, value : _t.Any) -> None:
+    def check(x : ReqresExpr[_t.Any], name : str, value : _t.Any) -> None:
         if x[name] != value:
             raise CatastrophicFailure("while evaluating %s of %s, expected %s, got %s", name, x.reqres.request.url, value, x[name])
 
@@ -790,7 +818,7 @@ def check_request_response(cmd : str, part : str) -> bool:
         raise CatastrophicFailure("`%s`: unexpected argument, expected `request` or `response`, got `%s`", cmd, part)
     return part == "request"
 
-def check_rrexpr(cmd : str, rrexpr : _t.Any) -> ReqresExpr:
+def check_rrexpr(cmd : str, rrexpr : _t.Any) -> ReqresExpr[_t.Any]:
     if not isinstance(rrexpr, ReqresExpr):
         typ = type(rrexpr)
         raise CatastrophicFailure("`%s`: expecting `ReqresExpr` value as the command environment, got `%s`", cmd, typ.__name__)
@@ -799,7 +827,7 @@ def check_rrexpr(cmd : str, rrexpr : _t.Any) -> ReqresExpr:
 def check_scrub(opts : str, url : str, ct : str, headers : Headers, data : str, eres : str) -> None:
     def sc(sniff : bool) -> None:
         t = trivial_Reqres(parse_url(url), ct, sniff=sniff, headers=headers, data=data.encode("utf-8"))
-        x = ReqresExpr(t, None, [])
+        x = ReqresExpr(UnknownSource(), t)
 
         res = x[f"response.body|eb|scrub response {opts}"].decode("utf-8")
         if res != eres:

tool/hoardy_web/io.py
@@ -20,10 +20,11 @@ import errno as _errno
 import io as _io
 import os as _os
 import shutil as _shutil
+import stat as _stat
 import sys as _sys
 import typing as _t
 
-from gettext import gettext, ngettext
+from gettext import gettext
 
 from kisstdlib.exceptions import *
 
@@ -272,107 +273,3 @@ def atomic_write(data : bytes, dst : _t.AnyStr,
     except OSError as exc:
         handle_ENAMETOOLONG(exc, dst)
         raise exc
-
-DataSource = _t.TypeVar("DataSource")
-class DeferredIO(_t.Generic[DataSource, _t.AnyStr]):
-    """A deferred IO operation over abs_out_path : _.AnyStr, which uses a DataSource
-       that has ReqresExpr inside as the source of data to be IO'd.
-
-       This exists for efficiently reasons: to eliminate away consequent
-       `os.rename` and `os.symlink` calls to the same target when updating FS
-       paths, and also so that disk writes could be batched.
-    """
-
-    def format_source(self) -> str | bytes:
-        raise NotImplementedError()
-
-    def approx_size(self) -> int:
-        raise NotImplementedError()
-
-    @staticmethod
-    def defer(abs_out_path : _t.AnyStr, old_source : DataSource | None, new_source: DataSource) \
-            -> tuple[_t.Any | None, DataSource | None, bool]:
-            #        ^ this is _t.Self
-        raise NotImplementedError()
-
-    def update_from(self, new_source : DataSource) -> tuple[DataSource | None, bool]:
-        raise NotImplementedError()
-
-    def run(self, abs_out_path : _t.AnyStr,
-            dsync : DeferredSync | None = None,
-            dry_run : bool = False) \
-            -> DataSource | None:
-        raise NotImplementedError()
-
-@_dc.dataclass
-class SourcedBytes(_t.Generic[_t.AnyStr]):
-    source : _t.AnyStr
-    data : bytes
-
-    def approx_size(self) -> int:
-        return 64 + len(self.source) + len(self.data)
-
-    def format_source(self) -> _t.AnyStr:
-        return self.source
-
-def make_DeferredFileWriteIntent(allow_updates : bool) -> type:
-    @_dc.dataclass
-    class DeferredFileWrite(DeferredIO[SourcedBytes[_t.AnyStr], _t.AnyStr], _t.Generic[_t.AnyStr]):
-        source : SourcedBytes[_t.AnyStr]
-        exists : bool
-        updated : bool = _dc.field(default = False)
-
-        def format_source(self) -> str | bytes:
-            return self.source.format_source()
-
-        def approx_size(self) -> int:
-            return 32 + self.source.approx_size()
-
-        @staticmethod
-        def defer(abs_out_path : _t.AnyStr,
-                  old_source : SourcedBytes[_t.AnyStr] | None,
-                  new_source : SourcedBytes[_t.AnyStr]) \
-                -> tuple[_t.Any | None, SourcedBytes[_t.Any], bool]:
-            if old_source is None:
-                try:
-                    with open(abs_out_path, "rb") as f:
-                        old_source = SourcedBytes(abs_out_path, f.read())
-                except FileNotFoundError:
-                    return DeferredFileWrite(new_source, False), new_source, True
-                except OSError as exc:
-                    handle_ENAMETOOLONG(exc, abs_out_path)
-                    raise exc
-
-            intent = DeferredFileWrite(old_source, True)
-            source, permitted = intent.update_from(new_source)
-            if source is old_source:
-                return None, source, permitted
-            else:
-                return intent, source, permitted
-
-        def update_from(self, new_source : SourcedBytes[_t.AnyStr]) \
-                -> tuple[SourcedBytes[_t.AnyStr], bool]:
-            if self.source.data == new_source.data:
-                # same data
-                return self.source, True
-
-            if not allow_updates:
-                return self.source, False
-
-            # update
-            self.source = new_source
-            self.updated = True
-            return self.source, True
-
-        def run(self, abs_out_path : _t.AnyStr,
-                dsync : DeferredSync | None = None,
-                dry_run : bool = False) \
-                -> SourcedBytes[_t.AnyStr] | None:
-            if dry_run or (self.updated and file_content_equals(abs_out_path, self.source.data)):
-                # nothing to do
-                return self.source
-
-            atomic_write(self.source.data, abs_out_path, False, dsync = dsync)
-            return self.source
-
-    return DeferredFileWrite

tool/hoardy_web/mitmproxy.py
@@ -113,7 +113,7 @@ def mitmproxy_load_flow(flow : mitmproxy.http.HTTPFlow) -> Reqres | None:
 
     return Reqres(1, "hoardy-mitmproxy/1", rq.http_version, request, response, finished_at, {}, wsstream)
 
-def rrexprs_mitmproxy_load_fileobj(fobj : _io.BufferedReader, path : str | bytes) -> _t.Iterator[ReqresExpr]:
+def rrexprs_mitmproxy_load_fileobj(fobj : _io.BufferedReader, source : DeferredSourceType) -> _t.Iterator[ReqresExpr[StreamElementSource[DeferredSourceType]]]:
     stream = mitmproxy.io.FlowReader(fobj).stream()
     n = 0
     for flow in stream:
@@ -123,9 +123,9 @@ def rrexprs_mitmproxy_load_fileobj(fobj : _io.BufferedReader, path : str | bytes
         reqres = mitmproxy_load_flow(flow)
         if reqres is None:
             continue
-        yield ReqresExpr(reqres, path, [n])
+        yield ReqresExpr(StreamElementSource(source, n), reqres)
         n += 1
 
-def rrexprs_mitmproxy_loadf(path : str | bytes) -> _t.Iterator[ReqresExpr]:
+def rrexprs_mitmproxy_loadf(path : str | bytes) -> _t.Iterator[ReqresExpr[StreamElementSource[FileSource]]]:
     with open(path, "rb") as f:
-        yield from rrexprs_mitmproxy_load_fileobj(f, path)
+        yield from rrexprs_mitmproxy_load_fileobj(f, make_FileSource(path, f))

tool/hoardy_web/source.py
@@ -1,0 +1,153 @@
+# Copyright (c) 2024 Jan Malakhovski <oxij@oxij.org>
+#
+# This file is a part of `hoardy-web` project.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import abc as _abc
+import dataclasses as _dc
+import io as _io
+import os as _os
+import typing as _t
+
+from gettext import gettext
+
+from kisstdlib.exceptions import *
+
+from .io import *
+
+class DeferredSource(metaclass=_abc.ABCMeta):
+    @_abc.abstractmethod
+    def approx_size(self) -> int:
+        raise NotImplementedError()
+
+    @_abc.abstractmethod
+    def show_source(self) -> str:
+        raise NotImplementedError()
+
+    @_abc.abstractmethod
+    def get_fileobj(self) -> _io.BufferedReader:
+        raise NotImplementedError()
+
+    def get_bytes(self) -> bytes:
+        with self.get_fileobj() as f:
+            return f.read()
+
+    def same_as(self, other : _t.Any) -> bool:
+        return False
+
+    def replaces(self, other : _t.Any) -> bool:
+        return True
+
+class UnknownSource(DeferredSource):
+    def approx_size(self) -> int:
+        return 8
+
+    def show_source(self) -> str:
+        return f"<{id(self)}>"
+
+    def get_fileobj(self) -> _io.BufferedReader:
+        raise NotImplementedError()
+
+    def get_bytes(self) -> bytes:
+        raise NotImplementedError()
+
+class _BytesIOReader(_io.BytesIO):
+    def peek(self, size : int = 0) -> bytes:
+        return self.getvalue()[self.tell():size]
+
+BytesIOReader = _t.cast(_t.Callable[[bytes], _io.BufferedReader], _BytesIOReader)
+
+@_dc.dataclass
+class BytesSource(DeferredSource):
+    data : bytes
+
+    def approx_size(self) -> int:
+        return 16 + len(self.data)
+
+    def get_fileobj(self) -> _io.BufferedReader:
+        return BytesIOReader(self.data)
+
+    def get_bytes(self) -> bytes:
+        return self.data
+
+    def replaces(self, other : DeferredSource) -> bool:
+        if isinstance(other, BytesSource) and \
+           self.data == other.data:
+            return False
+        return True
+
+@_dc.dataclass
+class FileSource(DeferredSource):
+    path : str | bytes
+    st_mtime_ns : int
+    st_dev : int
+    st_ino : int
+
+    def approx_size(self) -> int:
+        return 40 + len(self.path)
+
+    def show_source(self) -> str:
+        return fsdecode_maybe(self.path)
+
+    def get_fileobj(self) -> _io.BufferedReader:
+        fobj = open(self.path, "rb")
+        in_stat = _os.fstat(fobj.fileno())
+        if self.st_mtime_ns != in_stat.st_mtime_ns:
+            raise Failure("`%s` changed between accesses", self.path)
+        return fobj
+
+    def same_as(self, other : DeferredSource) -> bool:
+        if isinstance(other, FileSource) and \
+           self.st_ino != 0 and other.st_ino != 0 and \
+           self.st_dev == other.st_dev and \
+           self.st_ino == other.st_ino:
+            # same source file inode
+            return True
+        return False
+
+    def replaces(self, other : DeferredSource) -> bool:
+        if isinstance(other, FileSource) and self.path == other.path:
+            return False
+        return True
+
+def make_FileSource(path : str | bytes, fobj : _io.BufferedReader) -> FileSource:
+    in_stat = _os.fstat(fobj.fileno())
+    return FileSource(path, in_stat.st_mtime_ns, in_stat.st_dev, in_stat.st_ino)
+
+DeferredSourceType = _t.TypeVar("DeferredSourceType", bound=DeferredSource)
+
+@_dc.dataclass
+class StreamElementSource(DeferredSource, _t.Generic[DeferredSourceType]):
+    stream_source : DeferredSourceType
+    num : int
+
+    def approx_size(self) -> int:
+        return 24 + self.stream_source.approx_size()
+
+    def show_source(self) -> str:
+        return self.stream_source.show_source() + "//" + str(self.num)
+
+    def get_fileobj(self) -> _io.BufferedReader:
+        raise NotImplementedError()
+
+    def get_bytes(self) -> bytes:
+        raise NotImplementedError()
+
+    def replaces(self, other : DeferredSource) -> bool:
+        if isinstance(other, StreamElementSource) and \
+           self.stream_source == other.stream_source and \
+           self.num == other.num:
+            return False
+        return True

commit b1a774f277057b6db5a913bbd2fd56d31be8ff2c

tool: export mirror: fix overly large memory consumption by parsing input `WRR`s more carefully
... and unloading unneeded `Reqres` in more places

This returns memory consumption to what it was at `tool-v0.17.0`.

Bug introduced in 37c06af51212d89ab2c8446e8b1ab07a4f7e034d.

tool/hoardy_web/__main__.py
@@ -219,7 +219,7 @@ def map_wrr_paths(cargs : _t.Any,
             if not filters_allow(cargs, rrexpr): return
             emit_func(rrexpr)
 
-    map_wrr_paths_extra(cargs, rrexprs_wrr_bundle_loadf, emit_many, paths, **kwargs) # type: ignore # type inference fail
+    map_wrr_paths_extra(cargs, rrexprs_wrr_some_loadf, emit_many, paths, **kwargs) # type: ignore # type inference fail
 
 def get_bytes(value : _t.Any) -> bytes:
     if value is None or isinstance(value, (bool, int, float, Epoch)):
@@ -1273,7 +1273,7 @@ def make_deferred_emit(cargs : _t.Any,
                     raise exc
 
                 if updated_rrexpr is not None:
-                    updated_rrexpr.unload(True)
+                    updated_rrexpr.unload()
             else:
                 mem.consumption -= intent.approx_size() + len(abs_out_path)
                 permitted = intent.switch_source(new_rrexpr, moving) # (switchSource)
@@ -1507,14 +1507,15 @@ def cmd_export_mirror(cargs : _t.Any) -> None:
 
     @_dc.dataclass
     class Indexed:
+        stime : Epoch
         rrexpr : ReqresExpr[_t.Any]
         _abs_out_path : str | None = _dc.field(default = None)
 
         def __post_init__(self) -> None:
-            mem.consumption += 24 + self.rrexpr.approx_size()
+            mem.consumption += 32 + self.rrexpr.approx_size()
 
         def __del__(self) -> None:
-            mem.consumption -= 24 + self.rrexpr.approx_size() + \
+            mem.consumption -= 32 + self.rrexpr.approx_size() + \
                 (len(self._abs_out_path) if self._abs_out_path is not None else 0)
 
         @property
@@ -1594,16 +1595,19 @@ def cmd_export_mirror(cargs : _t.Any) -> None:
                response.code != 200:
                 return
 
+            stime = rrexpr.stime
             net_url = rrexpr.net_url
             iobj = index.get(net_url, None)
 
-            if iobj is not None and iobj.rrexpr.stime > rrexpr.stime:
+            if iobj is not None and iobj.stime > stime:
                 # the indexed one is newer
                 return
 
-            index[net_url] = iobj = Indexed(rrexpr)
+            index[net_url] = iobj = Indexed(stime, rrexpr)
 
+            enqueue = False
             if net_url in queue:
+                enqueue = True
                 queue[net_url] = iobj
             else:
                 enqueue = enqueue_all
@@ -1636,7 +1640,7 @@ def cmd_export_mirror(cargs : _t.Any) -> None:
                     queue[net_url] = iobj
                     report_queued(net_url, rrexpr.pretty_net_url if pretty_net_url is None else pretty_net_url, 1)
 
-            if mem.consumption > max_memory_mib:
+            if not enqueue or mem.consumption > max_memory_mib:
                 iobj.unload()
         return emit
 
@@ -1693,10 +1697,10 @@ def cmd_export_mirror(cargs : _t.Any) -> None:
         if level0:
             Stats.doc_n += 1
 
+        stime = iobj.stime
         rrexpr = iobj.rrexpr
         abs_out_path = iobj.abs_out_path
         source = rrexpr.source
-        stime = rrexpr.stime
         done[net_url] = abs_out_path
         mem.consumption += 16 + len(abs_out_path)
 
@@ -1775,22 +1779,24 @@ def cmd_export_mirror(cargs : _t.Any) -> None:
                             except KeyError: pass
                             # render it immediately
                             render(unet_url, uobj, enqueue, new_queue, level + 1)
+                            uobj.unload()
                         elif unet_url in new_queue or unet_url in queue:
                             # nothing to do
-                            pass
+                            if mem.consumption > max_memory_mib:
+                                uobj.unload()
                         elif enqueue:
                             new_queue[unet_url] = uobj
                             report_queued(unet_url, purl.pretty_net_url, level + 1)
+                            if mem.consumption > max_memory_mib:
+                                uobj.unload()
                         else:
                             # this will not be exported
                             uabs_out_path = None
+                            uobj.unload()
                             # NB: Not setting `done[unet_url]` here because it
                             # might be a requisite for another page. In which
                             # case this page will void this `unet_url`
                             # unnecessarily, yes.
-
-                        if mem.consumption > max_memory_mib:
-                            uobj.unload()
 
                 if uabs_out_path is None:
                     if fallbacks is not None:

tool/hoardy_web/wrr.py
@@ -250,18 +250,19 @@ def wrr_load_cbor_fileobj(fobj : _io.BufferedReader) -> Reqres:
 
 def wrr_load(fobj : _io.BufferedReader) -> Reqres:
     fobj = ungzip_fileobj_maybe(fobj)
-    res = wrr_load_cbor_fileobj(fobj)
-    p = fobj.peek(16)
+    if fobj.peek(1) == b"":
+        raise WRRParsingError(gettext("expected CBOR data, got EOF"))
+    reqres = wrr_load_cbor_fileobj(fobj)
+    p = fobj.peek(1)
     if p != b"":
         # there's some junk after the end of the Reqres structure
         raise WRRParsingError(gettext("expected EOF, got `%s`"), p)
-    return res
+    return reqres
 
 def wrr_bundle_load(fobj : _io.BufferedReader) -> _t.Iterator[Reqres]:
     fobj = ungzip_fileobj_maybe(fobj)
     while True:
-        p = fobj.peek(16)
-        if p == b"": break
+        if fobj.peek(1) == b"": break
         yield wrr_load_cbor_fileobj(fobj)
 
 def wrr_loadf(path : _t.AnyStr) -> Reqres:
@@ -536,7 +537,7 @@ class ReqresExpr(DeferredSource, _t.Generic[DeferredSourceType]):
         self._reqres = reqres
         return reqres
 
-    def unload(self, completely : bool = False) -> None:
+    def unload(self, completely : bool = True) -> None:
         if isinstance(self.source, FileSource):
             # this `reqres` is cheap to re-load
             self._reqres = None
@@ -687,6 +688,25 @@ def rrexprs_wrr_bundle_load(fobj : _io.BufferedReader, source : DeferredSourceTy
         yield ReqresExpr(StreamElementSource(source, n), reqres)
         n += 1
 
+def rrexprs_wrr_some_load(fobj : _io.BufferedReader, source : DeferredSourceType) -> _t.Iterator[ReqresExpr[DeferredSourceType | StreamElementSource[DeferredSourceType]]]:
+    fobj = ungzip_fileobj_maybe(fobj)
+    if fobj.peek(1) == b"":
+        raise WRRParsingError(gettext("expected CBOR data, got EOF"))
+
+    reqres = wrr_load_cbor_fileobj(fobj)
+    if fobj.peek(1) == b"":
+        yield ReqresExpr(source, reqres)
+        return
+
+    yield ReqresExpr(StreamElementSource(source, 0), reqres)
+
+    n = 1
+    while True:
+        reqres = wrr_load_cbor_fileobj(fobj)
+        yield ReqresExpr(StreamElementSource(source, n), reqres)
+        n += 1
+        if fobj.peek(1) == b"": break
+
 def rrexpr_wrr_loadf(path : str | bytes, in_stat : _os.stat_result | None = None) -> ReqresExpr[FileSource]:
     with open(path, "rb") as f:
         return rrexpr_wrr_load(f, make_FileSource(path, f, in_stat))
@@ -694,6 +714,10 @@ def rrexpr_wrr_loadf(path : str | bytes, in_stat : _os.stat_result | None = None
 def rrexprs_wrr_bundle_loadf(path : str | bytes, in_stat : _os.stat_result | None = None) -> _t.Iterator[ReqresExpr[StreamElementSource[FileSource]]]:
     with open(path, "rb") as f:
         yield from rrexprs_wrr_bundle_load(f, make_FileSource(path, f, in_stat))
+
+def rrexprs_wrr_some_loadf(path : str | bytes, in_stat : _os.stat_result | None = None) -> _t.Iterator[ReqresExpr[FileSource | StreamElementSource[FileSource]]]:
+    with open(path, "rb") as f:
+        yield from rrexprs_wrr_some_load(f, make_FileSource(path, f, in_stat))
 
 def trivial_Reqres(url : ParsedURL,
                    content_type : str = "text/html",