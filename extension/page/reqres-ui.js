5e378c4ac80ca9e5fb2e73e7cd7c172e0019a7c1
Jan Malakhovski <oxij@oxij.org>
2026-04-30 14:00:00 +0000

    extension/page/reqres-ui.js: columns: merge "WRR" and "pWA" into "Info", improve its string format, rename "Reason" -> "R", improve help strings

--- extension/page/reqres-ui.js
+++ extension/page/reqres-ui.js
@@ -48,24 +48,25 @@ let showStateFuncMap = new Map();
 function appendLoggable(el, loggable) {
     let tr = document.createElement("tr");
 
-    let sparts = [];
+    let sparts = [loggable.status];
     let color;
     if (loggable.collected === true) {
         color = "collected";
-        sparts.push("collected");
+        sparts.push("@collected");
     } else if (loggable.collected === false) {
         color = "discarded";
-        sparts.push("discarded");
+        sparts.push("@discarded");
     } else if (loggable.picked === true) {
         color = "picked";
-        sparts.push("picked");
+        sparts.push("@picked");
     } else if (loggable.picked === false) {
         color = "dropped";
-        sparts.push("dropped");
+        sparts.push("@dropped");
     } else {
         color = "in-flight";
-        sparts.push("in_flight");
+        sparts.push("@in_flight");
     }
+
     tr.classList.add(color);
 
     if (loggable.net_state !== undefined)
@@ -78,14 +79,15 @@ function appendLoggable(el, loggable) {
         sparts.push("buggy");
     if (loggable.redirectUrl !== undefined)
         sparts.push("redirected");
-    if (loggable.in_limbo === true)
-        sparts.push("in_limbo");
-    else if (loggable.was_in_limbo === true)
-        sparts.push("was_in_limbo");
     if (loggable.problematic === true)
         sparts.push("problematic!");
     else if (loggable.was_problematic === true)
         sparts.push("was_problematic");
+
+    if (loggable.in_limbo === true)
+        sparts.push("@in_limbo");
+    else if (loggable.was_in_limbo === true)
+        sparts.push("was_in_limbo");
 
     function mn(node, inn, data) {
         let n = document.createElement(inn);
@@ -136,8 +138,6 @@ function appendLoggable(el, loggable) {
     td.appendChild(div);
     tr.appendChild(td);
 
-    mtr(loggable.status);
-
     mtr(sparts.join(" "));
     mtr(dateToString(loggable.requestTimeStamp));
     mtr(loggable.protocol);
@@ -156,12 +156,11 @@ function appendLoggable(el, loggable) {
         etr.classList.add("errors");
 
         let etd = document.createElement("td");
-        etd.setAttribute("colspan", 2);
         etr.appendChild(etd);
 
         etd = document.createElement("td");
         etd.classList.add(color);
-        etd.setAttribute("colspan", 7);
+        etd.setAttribute("colspan", 8);
         etd.setAttribute("title", "errors");
         etd.innerHTML = escapeHTML(loggable.errors.join("\n")).replaceAll("\n", "<br>");
         etr.appendChild(etd);
@@ -190,15 +189,41 @@ function resetDataNode(id, log_data, predicate) {
 }
 
 const headerHTML = `
-<th><span data-help="Source of this reqres: &quot;ext&quot; for reqres produced by extensions, &quot;bg&quot; for reqres produced by background tasks, &quot;tab #N&quot; for reqres produced by the tab with id \`N\`, optionally followed by &quot;of *S&quot; where \`S\` is the last three digits of \`.sessionId\`. For tabs of the current session the label is a button which switches currently active tab to the tab in question. If the current page is not narrowed to a session and the reqres in question belongs to an older session, then a button labled &quot;S&quot; follows. That button opens another state page narrowed to the session in question. Otherwise, if the current page is not narrowed to a tab, a button labled &quot;T&quot; or &quot;ST&quot; follows. That buton open another state page narrowed to the tab (and session) in question.">Src</span></th>
-<th><span data-help="The \`.status\` this reqres will have in \`hoardy\`: &quot;I&quot; or &quot;C&quot; character (for &quot;Incomplete&quot; and &quot;Complete&quot; respectively) representing the value of \`.request.complete\` flag followed by either &quot;N&quot; (for &quot;No response&quot;) or an HTTP status code (integer, e.g. &quot;200&quot;), followed by &quot;I&quot; or &quot;C&quot; representing the value of \`.response.complete\` flag.">WRR</span></th>
-<th><span data-help="The current reqres \`state\` followed by quot;$&quot; symbol prepended to the \`final networking state\`, followed by &quot;partial&quot; when this reqres has partial request body (not \`.request.complete\`), followed by &quot;incomplete&quot; when it has incomplete response body (not \`.response.compete\`), followed by &quot;buggy&quot; when it has buggy metadata, followed &quot;redirected&quot; when it is a redirect, followed by either &quot;in_limbo&quot; or &quot;was_in_limbo&quot; if it is or was in limbo, followed by either &quot;problematic!&quot; or &quot;was_problematic&quot; if it is or was marked as \`problematic\`. See the \`Help\` page for more info.">pWA</span></th>
+<th><span data-help="Source of this reqres:
+
+- &quot;ext&quot; for reqres produced by extensions,
+- &quot;bg&quot; for reqres produced by background tasks,
+- &quot;tab #N&quot; for reqres produced by the tab with id \`N\`, optionally followed by &quot;of *S&quot where \`S\` is the last three digits of reqres' \`sessionId\`.
+
+For reqres belonging to tabs of the current session the label becomes a button which switches currently active tab to the tab in question.
+
+If the current page is not narrowed to a session and the reqres in question belongs to an older session, then a button labled &quot;S&quot; follows. That button opens another state page narrowed to the session in question.
+
+Otherwise, if the current page is not narrowed to a tab, a button labled &quot;T&quot; or &quot;ST&quot; follows. That buton open another state page narrowed to the tab (and session) in question.">Src</span></th>
+<th><span data-help="State-related information of this reqres.
+
+The \`status\` uses the same format as \`status\` in \`hoardy-web\`: &quot;I&quot; or &quot;C&quot; character (for &quot;Incomplete&quot; and &quot;Complete&quot; respectively) representing the value of \`request.complete\` followed by either &quot;N&quot; (for &quot;No response&quot;) or an HTTP status code (integer, e.g. &quot;200&quot;) which is followed by &quot;I&quot; or &quot;C&quot; (same as above) representing the value of \`response.complete\`.
+
+After that comes one of &quot;@&quot; symbol prepended to the name of the \`current state\`.
+
+After that comes &quot;$&quot; symbol prepended to the name of the \`final networking state\`.
+
+That can be followed by
+- &quot;partial&quot; when this reqres has partial request body (not \`request.complete\`);
+- &quot;incomplete&quot; when it has incomplete response body (not \`response.compete\`);
+- &quot;buggy&quot; when it has buggy metadata;
+- &quot;redirected&quot; when it is a redirect;
+- either &quot;problematic!&quot; or &quot;was_problematic&quot; if it is or was marked as \`problematic\`;
+- either &quot;@in_limbo&quot; or &quot;was_in_limbo&quot; if it is or was in limbo.
+
+See the \`Help\` page for more info.
+">Info</span></th>
 <th><span data-help="Timestamp of when the first byte of HTTP request headers was sent.">Request at</span></th>
 <th><span data-help="Protocol/version.">P</span></th>
 <th><span data-help="Protocol method.">M</span></th>
 <th><span data-help="Request URL, followed by &quot; -> &quot; and a redirect URL when this reqres is a redirect.">URL</span></th>
 <th><span data-help="Timestamp of when the first byte of HTTP response headers was received.">Response at</span></th>
-<th><span data-help="HTTP protocol response reason, if any. Note that the HTTP response code is displayed as a part of the &quot;WRR&quot; field.">Reason</span></th>
+<th><span data-help="HTTP protocol response reason, if any. Note that the HTTP response code is displayed as a part of the &quot;Info&quot; field.">R</span></th>
 `;
 
 async function commonMain() {
