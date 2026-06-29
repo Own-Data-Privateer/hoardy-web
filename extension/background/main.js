Own-Data-Privateer/hoardy-web

commit 89562f6d64a78a08568398136aca3b7201e6230b

extension/background/{main, reload}.js: implement save and restore for per-tab/per-source and background states

extension/background/main.js
@@ -687,6 +687,7 @@ async function main() {
     // and its elements in `reqresProblematic`.
 
     let oldSession = localData.session;
+    let sessionBg;
     let sessionTabs = {};
     if (oldSession !== undefined) {
         // to prevent it from being loaded again
@@ -694,18 +695,20 @@ async function main() {
 
         sessionId = oldSession.id;
         console.log(`MAIN: Loading old session ${oldSession.id}`);
-        sessionTabs = oldSession.tabs;
+        sessionBg = oldSession.bg;
+        sessionTabs = oldSession.tabs || {};
         reqresLog = oldSession.log;
+    }
 
-        // populate reqresProblematic
-        for (let loggable of reqresLog) {
-            if (loggable.problematic) {
-                let tabstate = getTabState(loggable.tabId, loggable.fromExtension);
-                reqresProblematic.push([loggable, null]);
-                tabstate.problematicTotal += 1;
-                gotNewProblematic = true;
-            }
-        }
+    // Restore the state of background requests.
+
+    if (sessionBg !== undefined) {
+        let bgstate = updateFromRec(assignRec({}, tabStateDefaults), sessionBg, {
+            problematicTotal: 0,
+            inLimboTotal: 0,
+            inLimboSize: 0,
+        });
+        tabState.set(-1, bgstate);
     }
 
     // Init configs and states of currently open tabs, possibly reusing old session data.
@@ -718,17 +721,36 @@ async function main() {
         // record they exist
         openTabs.add(tabId);
 
-        let oldTab = sessionTabs[tab.id];
+        let tabcfg = prefillChildren(configDefaults.root);
+        let tabstate = assignRec({}, tabStateDefaults);
 
-        let tabcfg = prefillChildren(config.root);
-        if (oldTab !== undefined && oldTab.url === tabUrl)
-            // reuse old config
-            tabcfg = updateFromRec(tabcfg, oldTab.tabcfg);
+        let oldTab = sessionTabs[tabId];
+        if (oldTab !== undefined && oldTab.url === tabUrl) {
+            // reuse old values
+            tabcfg = updateFromRec(tabcfg, getFirstDefined(oldTab.cfg, oldTab.tabcfg));
+            tabstate = updateFromRec(tabstate, oldTab.state, {
+                problematicTotal: 0,
+                inLimboTotal: 0,
+                inLimboSize: 0,
+            });
+        }
+
         setTabConfig(tabId, undefined, tabcfg, tabcfg, true);
+        tabState.set(tabId, tabstate);
 
         // on Chromium, reset their URLs, maybe
         if (useDebugger && tabUrl === "chrome://newtab/")
             chromiumResetRootTab(tabId, tabcfg);
+    }
+
+    // Populate reqresProblematic.
+    for (let loggable of reqresLog) {
+        if (loggable.problematic) {
+            let tabstate = getTabState(loggable.tabId, loggable.fromExtension);
+            reqresProblematic.push([loggable, null]);
+            tabstate.problematicTotal += 1;
+            gotNewProblematic = true;
+        }
     }
 
     // Init capture.

extension/background/reload.js
@@ -85,20 +85,22 @@ async function performReloadSelf() {
 
     console.warn("reloading!");
 
-    let tabs = {};
-    let currentTabs = await browser.tabs.query({});
+    let savedTabs = {};
 
+    let currentTabs = await browser.tabs.query({});
     for (let tab of currentTabs) {
         let tabId = tab.id;
-        tabs[tabId] = {
+        savedTabs[tabId] = {
             url: getTabURL(tab),
-            tabcfg: tabConfig.get(tabId),
+            cfg: tabConfig.get(tabId),
+            state: tabState.get(tabId),
         };
     }
 
     let session = {
         id: sessionId,
-        tabs,
+        tabs: savedTabs,
+        bg: tabState.get(-1),
         log: reqresLog,
         // queue and others are stashed
     };

commit 5959c51b0be29e0e931ce98052792d55a67fed53

extension/background/main.js: main: use `config.root` as a base for per-tab configs
This fixes a bug where the first tab in a new browser starts with the wrong
values, which is a bug introduced in 89562f6d64a78a08568398136aca3b7201e6230b.

extension/background/main.js
@@ -776,7 +776,7 @@ async function main() {
         // record they exist
         openTabs.add(tabId);
 
-        let tabcfg = prefillChildren(configDefaults.root);
+        let tabcfg = prefillChildren(config.root);
         let tabstate = assignRec({}, tabStateDefaults);
 
         let oldTab = sessionTabs[tabId];