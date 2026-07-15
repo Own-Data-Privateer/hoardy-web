Own-Data-Privateer/hoardy-web

commit c24ed5ac80e3b4c98158747259fddb134daad1de

extension/page/help.js: helpMain: split into a bunch of functions, start updating the page when shortcuts change (when supported)

extension/page/help.js
@@ -23,55 +23,43 @@
 
 "use strict";
 
-async function helpMain () {
-    setPageLoading();
+let minWidth = 1355; // see ./help.template
+let columns = true;
 
-    let body = document.getElementById("body");
-    let iframe = document.getElementById("iframe");
-    let minWidth = 1355; // see ./help.template
-    let columns = true;
+// Resize elements to window. This switches between `columns` and `linear` layouts depending on
+// width. This part is not done via `CSS` because we use the `columns` value above too.
+function resize() {
+    let w = window.innerWidth;
+    let h = window.innerHeight;
 
-    // allow to un-highlight currently highlighted node
-    document.body.addEventListener("click", (event) => {
-        highlightNode(null);
-        broadcastToPopup("highlightNode", null);
-    });
+    console.log("current viewport:", w, h);
+    columns = w >= minWidth;
 
-    setupHistoryPopState();
+    setConditionalClass(document.body, "columns", columns);
+    setConditionalClass(document.body, "linear", !columns);
 
-    // no corresponding popup UI elements
-    let noPopup = new Set(["_execute_browser_action", "showLog", "showTabLog"]);
+    // Prevent independent scroll in `columns` layout.
+    let h1 = columns ? `${h - 5}px` : null;
+    body.style["min-height"]
+        = body.style["max-height"]
+        = body.style["height"]
+        = h1;
 
-    // generate shortcuts table
-    let tbody = document.getElementById("tbody-sk");
-    let shortcuts = await getShortcuts();
-    for (let [name, shortcut] of Object.entries(shortcuts)) {
-        if (name.startsWith("toggleTabConfig"))
-            name = mapShortcutName((name, children) => "div-tabconfig." + (children ? "children." : "") + name, name);
+    let ib = iframe.contentDocument.body;
+    if (ib === null)
+        // not yet loaded
+        return;
 
-        let desc = shortcut.description;
-        let [sdesc, ldesc] = desc.split(": ");
+    // Prevent in-iframe scroll in `linear` layout.
+    let h2 = columns ? h1 : `${ib.scrollHeight + 20}px`;
+    iframe.style["min-height"]
+        = iframe.style["max-height"]
+        = iframe.style["height"]
+        = h2;
+}
 
-        let cur = shortcut.shortcut;
-        let def = shortcut.suggested_key ? shortcut.suggested_key.default || "" : "";
-
-        let tr = document.createElement("tr");
-        appendElements(tr, "td", cur ? cur : "unbound");
-        appendElements(tr, "td", cur === def ? "ditto" : (def ? def : "unbound"));
-        if (noPopup.has(name))
-            appendElements(tr, "td", desc);
-        else
-            appendElements(tr, "td", [
-                [(e) => {
-                    e.href = `./popup.html#${name}`;
-                    return e;
-                }, "a", sdesc],
-                [": " + ldesc],
-            ]);
-        tbody.append(tr);
-    }
-
-    classifyDocumentLinks(document, [
+function updateLinks(node) {
+    classifyDocumentLinks(node, [
         ["/page/help.html", "internal"],
         ["/page/popup.html", "popup"],
         ["/", "local"],
@@ -118,39 +106,71 @@ async function helpMain () {
             };
         }
     });
+}
 
-    // Resize elements to window. This switches between `columns` and
-    // `linear` layouts depending on width. This part is not done via
-    // `CSS` because we use the `columns` value above too.
-    function resize() {
-        let w = window.innerWidth;
-        let h = window.innerHeight;
+// no corresponding popup UI elements
+let noPopup = new Set(["_execute_browser_action", "showLog", "showTabLog"]);
 
-        console.log("current viewport:", w, h);
-        columns = w >= minWidth;
+async function updatePage(initial) {
+    // generate shortcuts table
+    let rows = [];
 
-        setConditionalClass(document.body, "columns", columns);
-        setConditionalClass(document.body, "linear", !columns);
+    let shortcuts = await getShortcuts();
+    for (let [name, shortcut] of Object.entries(shortcuts)) {
+        if (name.startsWith("toggleTabConfig"))
+            name = mapShortcutName((name, children) => "div-tabconfig." + (children ? "children." : "") + name, name);
 
-        // Prevent independent scroll in `columns` layout.
-        let h1 = columns ? `${h - 5}px` : null;
-        body.style["min-height"]
-            = body.style["max-height"]
-            = body.style["height"]
-            = h1;
+        let desc = shortcut.description;
+        let [sdesc, ldesc] = desc.split(": ");
 
-        let ib = iframe.contentDocument.body;
-        if (ib === null)
-            // not yet loaded
-            return;
+        let cur = shortcut.shortcut;
+        let def = shortcut.suggested_key ? shortcut.suggested_key.default || "" : "";
 
-        // Prevent in-iframe scroll in `linear` layout.
-        let h2 = columns ? h1 : `${ib.scrollHeight + 20}px`;
-        iframe.style["min-height"]
-            = iframe.style["max-height"]
-            = iframe.style["height"]
-            = h2;
+        let tr = document.createElement("tr");
+        appendElements(tr, "td", cur ? cur : "unbound");
+        appendElements(tr, "td", cur === def ? "ditto" : (def ? def : "unbound"));
+        if (noPopup.has(name))
+            appendElements(tr, "td", desc);
+        else
+            appendElements(tr, "td", [
+                [(e) => {
+                    e.href = `./popup.html#${name}`;
+                    return e;
+                }, "a", sdesc],
+                [": " + ldesc],
+            ]);
+
+        rows.push(tr);
     }
+
+    let tbody = document.getElementById("tbody-sk");
+    tbody.replaceChildren(...rows);
+
+    if (!initial)
+        updateLinks(tbody);
+}
+
+async function helpMain () {
+    setPageLoading();
+
+    let body = document.getElementById("body");
+    let iframe = document.getElementById("iframe");
+
+    // allow to un-highlight currently highlighted node
+    document.body.addEventListener("click", (event) => {
+        highlightNode(null);
+        broadcastToPopup("highlightNode", null);
+    });
+
+    setupHistoryPopState();
+
+    if (browser.commands !== undefined && browser.commands.onChanged !== undefined)
+        browser.commands.onChanged.addListener(() => {
+            resetSingletonTimeout(scheduledUI, "updatePage", 300, () => updatePage(false));
+        });
+    await updatePage(true);
+
+    updateLinks(document);
 
     async function processUpdate(update) {
         let [what, data] = update;

commit b4ac72c6e7cb6746d342333c8a6c11ee725d5ef5

extension/page/help.js: fix layout computations
This fixes a bug where one-or-two-columns layout computations would crash.
A bug introduced in c24ed5ac80e3b4c98158747259fddb134daad1de.

Found by `oxlint`.

extension/page/help.js
@@ -23,6 +23,8 @@
 
 "use strict";
 
+let pbody;
+let iframe;
 let minWidth = 1355; // see ./help.template
 let columns = true;
 
@@ -40,7 +42,7 @@ function resize() {
 
     // Prevent independent scroll in `columns` layout.
     let h1 = columns ? `${h - 5}px` : null;
-    body.style["min-height"] = body.style["max-height"] = body.style["height"] = h1;
+    pbody.style["min-height"] = pbody.style["max-height"] = pbody.style["height"] = h1;
 
     let ib = iframe.contentDocument.body;
     if (ib === null) {
@@ -227,8 +229,8 @@ async function updatePage(initial) {
 async function helpMain() {
     setPageLoading();
 
-    let body = document.getElementById("body");
-    let iframe = document.getElementById("iframe");
+    pbody = document.getElementById("body");
+    iframe = document.getElementById("iframe");
 
     // allow to un-highlight currently highlighted node
     document.body.addEventListener("click", (event) => {
@@ -295,7 +297,7 @@ async function helpMain() {
     await sleep(1);
 
     // show UI
-    body.style["visibility"] = null;
+    pbody.style["visibility"] = null;
     document.getElementById("container").style["visibility"] = null;
 
     // finish