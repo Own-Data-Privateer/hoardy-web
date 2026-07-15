Own-Data-Privateer/hoardy-web

commit 23ebd092be8e5dd50fdb5bd5bc4ead3826cbf3a5

extension/{background/{main, state-tab}.js, manifest-commands.json, page/popup.*}: split `spawnChildTab`, `spawnReplay`, and `spawnNegated` away from `handleMenuAction`, introduce `spawnReplayTabBackward` keyboard shortcut and its popup UI button

extension/background/main.js
@@ -195,14 +195,14 @@ async function replay(query, direction) {
             isBoringOrServerURL(url)
         ) {
             if (config.debugRuntime)
-                console.log("MAIN: NOT replaying tab", tabId, url);
+                console.log("MAIN: NOT replaying tabId", tabId, "url", url);
             continue;
         }
 
         let replayURL = latestReplayOf(url);
 
         if (config.debugRuntime)
-            console.log("MAIN: replaying tab", tabId, url, "->", replayURL);
+            console.log("MAIN: replaying tabId", tabId, "url", url, "->", replayURL);
 
         await runWhenTabSettles(
             "replay", `replay tab #${tabId} (${url.substr(0, 80)})`,
@@ -214,6 +214,30 @@ async function replay(query, direction) {
     }
 
     scheduleEndgame(updatedTabId);
+}
+
+function spawnReplay(url, direction, newWindow, tab) {
+    if (!checkReplay())
+        return;
+
+    let newURL = url;
+
+    if (isBoringOrServerURL(url)) {
+        if (config.debugRuntime)
+            console.log("MAIN: spawn-cloning from tabId", tab.id, "url", url);
+    } else {
+        newURL = latestReplayOf(url);
+
+        if (config.debugRuntime)
+            console.log("MAIN: spawn-replaying from tabId", tab.id, "url", url, "->", newURL);
+    }
+
+    spawnChildTab(newURL, newWindow, tab);
+}
+
+function spawnNegated(url, newWindow, tab) {
+    negateConfigFor.add(tab.id);
+    spawnChildTab(newURL, newWindow, tab);
 }
 
 // Handlers.
@@ -336,6 +360,11 @@ let rpcCommands = {
     replay: (tabId, direction) => {
         replay(tabId, direction);
     },
+    spawnReplay: (tabId, direction) => {
+        browser.tabs.get(tabId).then((tab) => {
+            spawnReplay(getTabURL(tab), false, false, tab);
+        });
+    },
 
     getInFlight,
     stopInFlight: (rrfilter, reason) => {
@@ -448,6 +477,7 @@ let shortcutCommands = {
     // TODO rename -> *Backward
     replayTabBack: (tabId) => rpcCommands.replay(tabId, false),
     replayTabForward: (tabId) => rpcCommands.replay(tabId, true),
+    spawnReplayTabBackward: (tabId) => rpcCommands.spawnReplay(tabId, false),
 
     forgetAllTabLog: (tabId) => runThenScheduleEndgame(syncForgetLog, {tabId}),
     showTabState: (tabId, activeTabId) => showState(sessionId, null, tabId, "top", activeTabId),
@@ -587,42 +617,14 @@ function handleMenuAction(info, tab) {
     if (config.debugRuntime)
         console.log("BROWSER: MENU: request", info, "in tab", tab);
 
-    let tabId = tab.id;
-    let windowId = tab.windowId;
-
     let cmd = info.menuItemId;
     let newWindow = cmd.endsWith("-window");
     let url = info.linkUrl;
 
-    if (cmd.startsWith("replay-")) {
-        if (isBoringOrServerURL(url)) {
-            if (config.debugRuntime)
-                console.log("MAIN: spawn-cloning from tabId", tabId, "url", url);
-        } else if (!checkReplay()) {
-            return;
-        } else {
-            url = latestReplayOf(url);
-
-            if (config.debugRuntime)
-                console.log("MAIN: spawn-replaying from tabId", tabId, "url", url, "->", newURL);
-        }
-    } else if (cmd.startsWith("open-not-"))
-        negateConfigFor.add(tabId);
-
-    // See (openerIdsWorkaround).
-    openerIds.push([windowId, tabId]);
-
-    if (newWindow)
-        browser.windows.create({
-            url,
-            incognito: tab.incognito,
-        }).catch(logError);
+    if (cmd.startsWith("replay-"))
+        spawnReplay(url, false, newWindow, tab);
     else
-        browser.tabs.create({
-            url,
-            windowId,
-            openerTabId: tabId,
-        }).catch(logError);
+        spawnNegated(url, newWindow, tab);
 }
 
 function initMenus() {

extension/background/state-tab.js
@@ -358,6 +358,28 @@ function closeTabThenDiscardInLimbo(tabId) {
     });
 }
 
+// New tab spawning.
+
+function spawnChildTab(url, newWindow, tab) {
+    let windowId =  tab.windowId;
+    let tabId = tab.id;
+
+    // See (openerIdsWorkaround).
+    openerIds.push([windowId, tabId]);
+
+    if (newWindow && browser.windows !== undefined)
+        return browser.windows.create({
+            url,
+            incognito: tab.incognito,
+        }).catch(logError);
+
+    return browser.tabs.create({
+        url,
+        windowId,
+        openerTabId: tabId,
+    }).catch(logError);
+}
+
 // Tracking open tabs and generating their configs.
 
 let openTabs = new Set();
@@ -370,7 +392,7 @@ function processNewTab(tabId, windowId, openerTabId) {
     let openerWindowId = windowId;
 
     if (openerIds.length > 0)
-        // (openerIdsWorkaround)
+        // (openerIdsWorkaround):
         //
         // Work around the fact that `browser.windows.create` has no `openerTabId` argument.
         //

extension/page/popup.js
@@ -162,6 +162,7 @@ async function popupMain() {
         snapshotTab: "📸",
         replayTabBack: "⏮",
         //replayTabForward: "⏭",
+        spawnReplayTabBackward: "⏮🆕",
         forgetAllTabLog: "🧹",
         showTabState: "📜",
 
@@ -321,6 +322,7 @@ async function popupMain() {
         "snapshotTab",
         "replayTabBack",
         // "replayTabForward"
+        "spawnReplayTabBackward",
         "forgetAllTabLog",
 
         "collectAllTabInLimbo",

extension/page/popup.template
@@ -960,6 +960,7 @@ Specifically:
       <span class="right tag-def tag-tab">
         <input id="snapshotTab" type="button" data-shortcut="snapshotTab" value="Snapshot" />
         <input id="replayTabBack" type="button" class="on-replay" data-shortcut="replayTabBack" value="Replay" />
+        <input id="spawnReplayTabBackward" type="button" class="on-replay" data-shortcut="spawnReplayTabBackward" value="Replay+" />
         <!-- input id="replayTabForward" type="button" class="on-replay" data-shortcut="replayTabForward" data-help="" value="Replay Forward" / -->
         <input id="forgetAllTabLog" type="button" data-help="For reqres of this tab, forget recent history." value="Forget" />
         <input id="showTabState" type="button" tabindex=2 data-shortcut="showTabState" value="State" />

commit 53d45d4c0220470f59ef53de8ab389817605e9a3

extension/background/main.js: spawnNegated: fix
A bug introduced in 23ebd092be8e5dd50fdb5bd5bc4ead3826cbf3a5.

Found by `biome lint`.

extension/background/main.js
@@ -293,7 +293,7 @@ function spawnReplay(url, direction, newWindow, tab) {
 
 function spawnNegated(url, newWindow, tab) {
     negateConfigFor.add(tab.id);
-    spawnChildTab(newURL, newWindow, tab);
+    spawnChildTab(url, newWindow, tab);
 }
 
 // Handlers.